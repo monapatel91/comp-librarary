9877ca2b57b7e307203e60bcf9b75e86bc8eef36
Keith Stewart <kstewart@digital.ai>
2020-11-13 13:57:26 -0600

    S-73903: switch new components to use Controls

--- libs/dot-components/src/lib/progress/Progress.stories.tsx
+++ libs/dot-components/src/lib/progress/Progress.stories.tsx
@@ -1,71 +1,34 @@
 import React from 'react';
-import { number, select, text } from '@storybook/addon-knobs';
-import {
-  progressColorOptions,
-  DotProgress,
-  progressVariantOptions,
-} from './Progress';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { DotProgress, ProgressProps } from './Progress';
 
 export default {
-  component: DotProgress,
   title: 'Progress',
-};
+  component: DotProgress,
+  argTypes: {
+    color: { defaultValue: 'primary' },
+    size: {
+      defaultValue: 40,
+      control: { type: 'range', min: 15, max: 100, step: 1 },
+    },
+    thickness: {
+      defaultValue: 3.6,
+      control: { type: 'range', min: 1, max: 10, step: 0.5 },
+    },
+    title: {
+      defaultValue: 'Loading Data...',
+    },
+    variant: {
+      defaultValue: 'indeterminate',
+    },
+    value: {
+      defaultValue: 20,
+      control: { type: 'range', min: 0, max: 100, step: 1 },
+    },
+  },
+} as Meta;
 
-export const progress = () => {
-  const sbColorOptions = {
-    Inherit: 'inherit',
-    Primary: 'primary',
-    Secondary: 'secondary',
-  };
-  const sbVariantOptions = {
-    Determinate: 'determinate',
-    Indeterminate: 'indeterminate',
-    Static: 'static',
-  };
-
-  let groupId = 'Options';
-  const color = select(
-    'Color',
-    sbColorOptions,
-    'primary',
-    groupId
-  ) as progressColorOptions;
-  const size = number(
-    'Size',
-    40,
-    { range: true, min: 15, max: 100, step: 1 },
-    groupId
-  );
-  const thickness = number(
-    'Size',
-    3.6,
-    { range: true, min: 1, max: 10, step: 0.5 },
-    'Thickness'
-  );
-  const title = text('Tooltip', 'Loading Data...', groupId);
-  const variant = select(
-    'Variant',
-    sbVariantOptions,
-    'indeterminate',
-    groupId
-  ) as progressVariantOptions;
-
-  groupId = 'Static Options';
-  const value = number(
-    'Value',
-    20,
-    { range: true, min: 0, max: 100, step: 1 },
-    groupId
-  );
-
-  return (
-    <DotProgress
-      color={color}
-      size={size}
-      thickness={thickness}
-      title={title}
-      value={value}
-      variant={variant}
-    />
-  );
-};
+export const Primary: Story<ProgressProps> = (args) => (
+  <DotProgress {...args} />
+);
--- libs/dot-components/src/lib/table/LocallyPaginatedTable.stories.tsx
+++ libs/dot-components/src/lib/table/LocallyPaginatedTable.stories.tsx
@@ -1,49 +1,35 @@
 import React from 'react';
-import { boolean, object, radios, text } from '@storybook/addon-knobs';
-import LocallyPaginatedTable from './LocallyPaginatedTable';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import {
+  LocallyPaginatedTable,
+  LocallyPaginatedTableProps,
+} from './LocallyPaginatedTable';
 import { Header } from './TableHeader';
 import { Cell } from './TableCell';
 import { Order } from './TableBody';
 
 export default {
-  component: LocallyPaginatedTable,
   title: 'Paginated Table - Local',
-};
+  component: LocallyPaginatedTable,
+  argTypes: {
+    data: {
+      defaultValue: [
+        { title: 'Ironman', type: 'BITBUCKET' },
+        { title: 'Batman', type: 'GITHUB' },
+        { title: 'Captain Marvel', type: 'GITLAB' },
+        { title: 'Superman', type: 'TEAMFORGE' },
+      ],
+    },
+    columns: {
+      defaultValue: [
+        { id: 'title', label: 'Title' },
+        { id: 'type', label: 'Type' },
+      ],
+    },
+  },
+} as Meta;
 
-export const PaginatedTable = () => {
-  const orderByOptions = { Title: 'title', Type: 'type' };
-  const orderOptions = { Asc: 'asc', Desc: 'desc' };
-  const sampleColumns = [
-    { id: 'title', label: 'Title' },
-    { id: 'type', label: 'Type' },
-  ];
-  const sampleData = [
-    { title: 'Ironman', type: 'BITBUCKET' },
-    { title: 'Batman', type: 'GITHUB' },
-    { title: 'Captain Marvel', type: 'GITLAB' },
-    { title: 'Superman', type: 'TEAMFORGE' },
-  ];
-
-  let groupId = 'Options';
-  const ariaLabel = text('Aria Label', 'super heroes!', groupId);
-  const loading = boolean('Loading', false, groupId);
-  const order = radios('Order', orderOptions, 'asc', groupId) as Order;
-  const orderBy = radios('Order By', orderByOptions, 'title', groupId);
-  const sortable = boolean('Sortable', true, groupId);
-
-  groupId = 'Table Data';
-  const columns: Array<Header> = object('Columns', sampleColumns, groupId);
-  const data: Array<Cell> = object('Data', sampleData, groupId);
-
-  return (
-    <LocallyPaginatedTable
-      ariaLabel={ariaLabel}
-      columns={columns}
-      data={data}
-      defaultOrder={order}
-      defaultOrderBy={orderBy}
-      loading={loading}
-      sortable={sortable}
-    />
-  );
-};
+export const Primary: Story<LocallyPaginatedTableProps> = (args) => (
+  <LocallyPaginatedTable {...args} />
+);
