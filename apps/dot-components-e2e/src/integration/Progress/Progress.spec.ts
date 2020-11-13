68907816630500c35d874b008776997ad9432828
Matt Haff <mhaff@collab.net>
2020-11-13 09:35:00 -0500

    S-73847: omg progress just work

--- apps/dot-components-e2e/src/integration/Progress/Progress.spec.ts
+++ apps/dot-components-e2e/src/integration/Progress/Progress.spec.ts
@@ -2,6 +2,6 @@ describe('dot-components: Progress component', () => {
   beforeEach(() => cy.visit('/iframe.html?id=progress--progress'));
 
   it('should render the component', () => {
-    cy.get('table').should('contain', 'Sample Label');
+    cy.get('svg').should('have.class', 'MuiCircularProgress-svg');
   });
 });
