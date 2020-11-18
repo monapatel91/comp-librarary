14db9abc5b49572a9aecdd80de9109cf874eb460
Matt Haff <mhaff@collab.net>
2020-11-18 18:37:52 -0500

    S-73937: initial demo markup

--- apps/demo/src/app/app.tsx
+++ apps/demo/src/app/app.tsx
@@ -1,19 +1,61 @@
 import React from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 import './app.scss';
 
-import { DotButton, DotIcon } from '@digital-ai/dot-components';
+import {
+  DotHeader,
+  DotSidebar,
+  NavigationItemProps,
+} from '@digital-ai/dot-components';
+
+const topNavItems: Array<NavigationItemProps> = [
+  {
+    icon: 'notification-bell',
+    title: 'Alerts',
+    url: '/',
+  },
+  {
+    icon: 'help',
+    title: 'Help',
+    url: '/',
+  },
+  {
+    icon: 'apps',
+    title: 'Admin',
+    url: '/',
+  },
+  {
+    icon: 'dark',
+    onClick: () => console.log('theme clicked'),
+    title: 'Toggle Theme',
+    url: '',
+  },
+];
+
+const sideNavItems: Array<NavigationItemProps> = [
+  {
+    icon: 'block',
+    title: 'Home',
+    url: '/',
+  },
+  {
+    icon: 'change',
+    title: 'Changes',
+    url: '/',
+  },
+];
 
 export const App = () => {
   return (
-    <div>
-      <DotButton
-        displayText="Click"
-        onClick={() => alert('Button clicked!')}
-        type="primary"
-      />
-      <DotIcon icon="script" />
-    </div>
+    <BrowserRouter>
+      <DotHeader items={topNavItems} />
+      <main>
+        <DotSidebar primaryItems={sideNavItems} />
+        <section className="main-content">
+          <h1>I AM THE NIGHT</h1>
+        </section>
+      </main>
+    </BrowserRouter>
   );
 };
 
