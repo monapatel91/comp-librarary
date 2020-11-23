6422f94e77d53c760f8945531c69354e2dca2fd5
Keith Stewart <kstewart@digital.ai>
2020-11-23 15:00:47 -0600

    S-73961: init progression board

---
+++ libs/dot-components/src/lib/ctm/index.ts
@@ -0,0 +1,1 @@
+export { ProgressionBoard } from './progression-board/ProgressionBoard';
---
+++ libs/dot-components/src/lib/ctm/progression-board/ProgressionBoard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './ProgressionBoard.scss';
+
+/* eslint-disable-next-line */
+export interface ProgressionBoardProps {}
+
+export function ProgressionBoard(props: ProgressionBoardProps) {
+  return (
+    <div>
+      <h1>Welcome to ProgressionBoard!</h1>
+    </div>
+  );
+}
+
+export default ProgressionBoard;
