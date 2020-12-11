88370bc22ceb001abcac9503ae2577258af129c0
Keith Stewart <kstewart@digital.ai>
2020-12-11 03:59:54 -0600

    S-74097: fix e2e test

--- apps/dot-components-e2e/src/integration/Button/Button.spec.ts
+++ apps/dot-components-e2e/src/integration/Button/Button.spec.ts
@@ -2,7 +2,7 @@ describe('dot-components: Button component', () => {
   beforeEach(() => cy.visit('/iframe.html?id=components-button--default'));
 
   it('should have a dot- prefix', () => {
-    cy.get('button').should('have.class', 'dot-btn');
+    cy.get('button').should('have.class', 'dot-button');
   });
 
   it('should render the component', () => {
