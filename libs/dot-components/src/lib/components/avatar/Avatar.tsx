9b62cf678e360c92d09cd584400027b9f60ac221
Keith Stewart <kstewart@digital.ai>
2020-12-14 15:20:27 -0600

    S-73983: use common props

--- libs/dot-components/src/lib/components/avatar/Avatar.tsx
+++ libs/dot-components/src/lib/components/avatar/Avatar.tsx
@@ -3,19 +3,28 @@ import { Avatar } from '@material-ui/core';
 
 import './Avatar.scss';
 
-export interface AvatarProps {
+import { CommonProps } from '../CommonProps';
+
+export interface AvatarProps extends CommonProps {
   /** Text displayed on hover */
   alt?: string;
-  /** Space delimited CSS classes to be attributed to the avatar. */
-  classes?: string;
   /** Size of avatar displayed */
   size?: 'small' | 'medium';
 }
 
 // TODO: need to use src or srcSet in order to utilize alt text prop
-export const DotAvatar = ({ alt, classes, size = 'medium' }: AvatarProps) => {
+export const DotAvatar = ({
+  alt,
+  className,
+  'data-testid': dataTestId,
+  size = 'medium',
+}: AvatarProps) => {
   return (
-    <Avatar alt={alt} classes={{ root: `dot-avatar ${size} ${classes}` }} />
+    <Avatar
+      alt={alt}
+      data-testid={dataTestId}
+      classes={{ root: `dot-avatar ${size} ${className}` }}
+    />
   );
 };
 
