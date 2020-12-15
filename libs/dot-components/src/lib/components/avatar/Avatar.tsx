abe1ec1ec79daabb7f5fe47007968a4ef3260279
Keith Stewart <kstewart@digital.ai>
2020-12-15 14:34:30 -0600

    S-73983: introduce text type

--- libs/dot-components/src/lib/components/avatar/Avatar.tsx
+++ libs/dot-components/src/lib/components/avatar/Avatar.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Avatar, createStyles, Theme } from '@material-ui/core';
+import React, { ReactNode } from 'react';
+import { Avatar, createStyles, Theme, Typography } from '@material-ui/core';
+import { Variant } from '@material-ui/core/styles/createTypography';
 
 import { CommonProps } from '../CommonProps';
 import { useStylesWithRootClass } from '../makeStylesWithRootClass';
-import { DotIcon } from '../icon/Icon';
+import { DotIcon, IconFontSize } from '../icon/Icon';
 
 type AvatarSize = 'small' | 'medium' | 'large';
+type AvatarType = 'image' | 'text' | 'icon';
 
 const avatarSpacing = {
   small: 3,
@@ -50,6 +52,10 @@ export interface AvatarProps extends CommonProps {
   iconId?: string;
   /** Size of avatar displayed */
   size?: AvatarSize;
+  /** The text to be displayed. Only the first 2 letters will be displayed. */
+  text?: string;
+  /** The type of the avatar */
+  type?: AvatarType;
 }
 
 // TODO: need to use src or srcSet in order to utilize alt text prop
@@ -59,6 +65,8 @@ export const DotAvatar = ({
   'data-testid': dataTestId,
   iconId,
   size = 'medium',
+  text,
+  type = 'image',
 }: AvatarProps) => {
   const classes = useStylesWithRootClass('dot-avatar', styles, className, {
     size,
@@ -66,10 +74,45 @@ export const DotAvatar = ({
 
   // currently the icon component calls medium "default"
   const iconSize = size === 'medium' ? 'default' : size;
+  // determine values for variables dependent on size
+  let iconFontSize: IconFontSize;
+  let textVariant: Variant;
+  switch (size) {
+    case 'small':
+      iconFontSize = size;
+      textVariant = 'caption';
+      break;
+
+    case 'medium':
+      // this is just because currently the Icon component has a size
+      // called "default" rather than "medium"
+      iconFontSize = 'default';
+      textVariant = 'h3';
+      break;
+
+    case 'large':
+      iconFontSize = size;
+      textVariant = 'h1';
+      break;
+  }
+
+  let child: ReactNode;
+  switch (type) {
+    case 'icon':
+      child = <DotIcon icon={iconId} fontSize={iconFontSize} />;
+      break;
+
+    case 'image':
+      break;
+
+    case 'text':
+      child = <Typography variant={textVariant}>{text}</Typography>;
+      break;
+  }
 
   return (
     <Avatar alt={alt} data-testid={dataTestId} classes={{ ...classes }}>
-      {iconId ? <DotIcon icon={iconId} fontSize={iconSize} /> : null}
+      {child}
     </Avatar>
   );
 };
