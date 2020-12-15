121b1c763adb7ac96a459ee3ce2421be0f25ac77
Keith Stewart <kstewart@digital.ai>
2020-12-15 10:31:58 -0600

    S-73983: move styles to JSS

--- libs/dot-components/src/lib/components/avatar/Avatar.tsx
+++ libs/dot-components/src/lib/components/avatar/Avatar.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import { Avatar } from '@material-ui/core';
-
-import './Avatar.scss';
+import { Avatar, createStyles, Theme } from '@material-ui/core';
 
 import { CommonProps } from '../CommonProps';
+import { useStylesWithRootClass } from '../makeStylesWithRootClass';
+
+type AvatarSize = 'small' | 'medium';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      height: (props: { size: AvatarSize }) => {
+        switch (props.size) {
+          case 'small':
+            return theme.spacing(3);
+
+          default:
+            return theme.spacing(4);
+        }
+      },
+      width: (props: { size: AvatarSize }) => {
+        switch (props.size) {
+          case 'small':
+            return theme.spacing(3);
+
+          default:
+            return theme.spacing(4);
+        }
+      },
+    },
+  });
 
 export interface AvatarProps extends CommonProps {
   /** Text displayed on hover */
   alt?: string;
   /** Size of avatar displayed */
-  size?: 'small' | 'medium';
+  size?: AvatarSize;
 }
 
 // TODO: need to use src or srcSet in order to utilize alt text prop
@@ -19,13 +44,11 @@ export const DotAvatar = ({
   'data-testid': dataTestId,
   size = 'medium',
 }: AvatarProps) => {
-  return (
-    <Avatar
-      alt={alt}
-      data-testid={dataTestId}
-      classes={{ root: `dot-avatar ${size} ${className}` }}
-    />
-  );
+  const classes = useStylesWithRootClass('dot-avatar', styles, className, {
+    size,
+  });
+
+  return <Avatar alt={alt} data-testid={dataTestId} classes={{ ...classes }} />;
 };
 
 export default DotAvatar;
