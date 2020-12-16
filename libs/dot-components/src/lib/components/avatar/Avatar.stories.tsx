2e9e7d0125b998759ed0342b13961759014b3cdd
Keith Stewart <kstewart@digital.ai>
2020-12-15 20:51:04 -0600

    S-73983: add image example

--- libs/dot-components/src/lib/components/avatar/Avatar.stories.tsx
+++ libs/dot-components/src/lib/components/avatar/Avatar.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { DotAvatar, AvatarProps } from './Avatar';
+import EmptyState from '../../assets/empty-state.svg';
 
 export default {
   title: 'Components/Avatar',
@@ -9,3 +10,8 @@ export default {
 } as Meta;
 
 export const Default: Story<AvatarProps> = (args) => <DotAvatar {...args} />;
+
+export const Image: Story<AvatarProps> = ({
+  imageSrc = EmptyState,
+  ...rest
+}) => <DotAvatar imageSrc={imageSrc} {...rest} />;
