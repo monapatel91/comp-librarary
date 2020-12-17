2723c624cf84b565a3a9bc4dfd4a3d8ab0cb48b4
Keith Stewart <kstewart@digital.ai>
2020-12-17 13:38:21 -0600

    S-74144: define more thorough color palette

---
+++ libs/dot-components/src/lib/theme-provider/colors/light-theme-colors.ts
@@ -0,0 +1,127 @@
+//Primary blue
+//https://zeroheight.com/4a9ac476a/p/22005a-color/t/6288e9
+
+export const b50 = '#e8edf3';
+export const b100 = '#c5d3e2';
+export const b200 = '#9eb6cf';
+export const b300 = '#7798bb';
+export const b400 = '#5a82ad';
+export const b500 = '#3d6c9e';
+export const b600 = '#376496';
+export const b700 = '#2f598c';
+export const b800 = '#274f82';
+export const b900 = '#1a3d70';
+export const bA100 = '#abcaff';
+export const bA200 = '#78aaff';
+export const bA400 = '#458aff';
+export const bA700 = '#2b7aff';
+
+//Success green
+//https://zeroheight.com/4a9ac476a/p/22005a-color/t/653193
+
+export const g50 = '#e9f0e0';
+export const g100 = '#c8dab3';
+export const g200 = '#a4c280';
+export const g300 = '#80aa4d';
+export const g400 = '#649726';
+export const g500 = '#498500';
+export const g600 = '#427d00';
+export const g700 = '#397200';
+export const g800 = '#316800';
+export const g900 = '#215500';
+export const gA100 = '#aaff87';
+export const gA200 = '#86ff54';
+export const gA400 = '#61ff21';
+export const gA700 = '#4fff08';
+
+//Neutral color
+//https://zeroheight.com/4a9ac476a/p/22005a-color/t/44d174
+
+export const n50 = '#f3f5f6';
+export const n100 = '#e3e5e8';
+export const n200 = '#c1c6cd';
+export const n300 = '#a4acb6';
+export const n400 = '#667385';
+export const n500 = '#4b5a6f';
+export const n600 = '#445267';
+export const n700 = '#3b485c';
+export const n800 = '#333f52';
+export const n900 = '#232e40';
+export const nA100 = '#88b1ff';
+export const nA200 = '#5590ff';
+export const nA400 = '#226eff';
+export const nA700 = '#085eff';
+
+//Warning orange
+//https://zeroheight.com/4a9ac476a/p/22005a-color/t/01bbd7
+
+export const o50 = '#fff3e9';
+export const o100 = '#ffe2c8';
+export const o200 = '#ffcfa4';
+export const o300 = '#ffbb80';
+export const o400 = '#ffad64';
+export const o500 = '#ff9e49';
+export const o600 = '#ff9642';
+export const o700 = '#ff8c39';
+export const o800 = '#ff8231';
+export const o900 = '#ff7021';
+export const oA100 = '#ffffff';
+export const oA200 = '#ffffff';
+export const oA400 = '#ffe0d0';
+export const oA700 = '#ffceb7';
+
+//Error red
+//https://zeroheight.com/4a9ac476a/p/22005a-color/t/941bd8
+
+export const r50 = '#fae4e4';
+export const r100 = '#f3bcbc';
+export const r200 = '#eb8f90';
+export const r300 = '#e26264';
+export const r400 = '#dc4142';
+export const r500 = '#d61f21';
+export const r600 = '#d11b1d';
+export const r700 = '#cc1718';
+export const r800 = '#c61214';
+export const r900 = '#bc0a0b';
+export const rA100 = '#ffe7e7';
+export const rA200 = '#ffb4b5';
+export const rA400 = '#ff8182';
+export const rA700 = '#ff6869';
+
+//Graph color
+
+//Yellow
+//https://zeroheight.com/4a9ac476a/p/22005a-color/t/48416f
+
+export const y50 = '#fdf8e2';
+export const y100 = '#faeeb8';
+export const y200 = '#f7e288';
+export const y300 = '#f3d658';
+export const y400 = '#f1ce35';
+export const y500 = '#eec511';
+export const y600 = '#ecbf0f';
+export const y700 = '#e9b80c';
+export const y800 = '#e7b00a';
+export const y900 = '#e2a305';
+export const yA100 = '#ffffff';
+export const yA200 = '#fff2d7';
+export const yA400 = '#ffe2a4';
+export const yA700 = '#ffda8b';
+
+//Purple
+//https://zeroheight.com/4a9ac476a/p/22005a-color/t/48416f
+
+export const p50 = '#f3e4ee';
+export const p100 = '#e0bbd4';
+export const p200 = '#cc8eb8';
+export const p300 = '#b8609c';
+export const p400 = '#a83e86';
+export const p500 = '#991c71';
+export const p600 = '#911969';
+export const p700 = '#86145e';
+export const p800 = '#7c1154';
+export const p900 = '#6b0942';
+export const pA100 = '#ff9cd1';
+export const pA200 = '#ff69b9';
+export const pA400 = '#ff36a2';
+export const pA700 = '#ff1d96';
