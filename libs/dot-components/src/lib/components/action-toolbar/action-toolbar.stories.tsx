0fac7478602487e450a30eee72759ef951025569
Josh Schoen <jschoen@digital.ai>
2021-01-05 16:56:21 -0600

    fixed lint empty arrow error in stories

--- libs/dot-components/src/lib/components/action-toolbar/action-toolbar.stories.tsx
+++ libs/dot-components/src/lib/components/action-toolbar/action-toolbar.stories.tsx
@@ -17,7 +17,7 @@ export const Default: Story<DotActionBarProps> = (args) =>
   <DotActionToolbar {...args}>
     <Typography component="h1" variant="h4">Page title</Typography>
     <div>
-    <DotButton onClick={() => { }} label="Secondary" type="outlined" />
-    <DotButton onClick={() => { }} label="Primary" type="primary" />
+    <DotButton onClick={() => console.log('Secondary')} label="Secondary" type="outlined" />
+    <DotButton onClick={() => console.log('primary')} label="Primary" type="primary" />
     </div>
   </DotActionToolbar>
