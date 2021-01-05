85a4947000fd54b20a6095b19615d5796c9b492e
Matt Haff <mhaff@collab.net>
2021-01-05 13:59:33 -0500

    resolve failed e2e test, adjust spacing of margin next to logo when hamburger menu not available

--- apps/dot-components-e2e/src/integration/AppToolbar/AppToolbar.spec.ts
+++ apps/dot-components-e2e/src/integration/AppToolbar/AppToolbar.spec.ts
@@ -1,5 +1,5 @@
 describe('dot-components: AppToolbar component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=components-app-toolbar--default'));
+  beforeEach(() => cy.visit('/iframe.html?id=components-apptoolbar--default'));
 
   it('should have a dot- prefix', () => {
     cy.get('header').should('have.class', 'dot-app-toolbar');
