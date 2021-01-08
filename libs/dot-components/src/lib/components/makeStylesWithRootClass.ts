4e2377f899fcd3e3c7f28c9d6019696c8cdb7df8
Keith Stewart <kstewart@digital.ai>
2021-01-08 15:26:30 -0600

    #87: convert Button and Avatar components to styled components

--- libs/dot-components/src/lib/components/makeStylesWithRootClass.ts
+++ libs/dot-components/src/lib/components/makeStylesWithRootClass.ts
@@ -1,27 +1,6 @@
-/* eslint-disable @typescript-eslint/ban-types */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { makeStyles } from '@material-ui/core';
-import { Styles } from '@material-ui/styles/withStyles';
-
-export function useStylesWithRootClass<Theme, ClassKey extends string = string>(
-  name: string,
-  styles: Styles<Theme, {}, ClassKey>,
-  className?: string,
-  props?: any
-) {
-  // generate style classes
-  const classes = makeStyles(styles, { name })(props);
-
-  // get the root class from generated styles
-  const rootClass = 'root' in classes ? classes['root'] : '';
-
+export function useStylesWithRootClass(name: string, className?: string) {
   // combine the root classes into one string
-  const rootClasses = [name, rootClass, ...(className ? [className] : [])].join(
-    ' '
-  );
+  const rootClasses = [name, ...(className ? [className] : [])].join(' ');
 
-  // set the combined root classes on the classes object
-  classes['root'] = rootClasses;
-
-  return classes;
+  return rootClasses;
 }
