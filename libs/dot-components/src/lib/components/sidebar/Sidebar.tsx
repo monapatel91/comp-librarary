fa4052f3c68b53941aaa6ba4b8119db0f8f9e88e
Matt Haff <mhaff@collab.net>
2021-01-13 16:55:54 -0500

    issue #114: convert sidebar to styled component

--- libs/dot-components/src/lib/components/sidebar/Sidebar.tsx
+++ libs/dot-components/src/lib/components/sidebar/Sidebar.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
+import { Theme } from '@material-ui/core';
+import styled, { css } from 'styled-components';
 import { DotIconButton } from '../button/IconButton';
 import { DotNavigation } from '../navigation/Navigation';
+import { CommonProps } from '../CommonProps';
+import { useStylesWithRootClass } from '../makeStylesWithRootClass';
 import { NavigationItemProps } from '../navigation/NavItem';
 import { ReactComponent as LogoD } from '../../assets/logo_d.svg';
 import { ReactComponent as LogoDigitalAi } from '../../assets/logo_digital_ai.svg';
 
-import './Sidebar.scss';
-
-export interface SidebarProps {
+export interface SidebarProps extends CommonProps {
   backItem?: Array<NavigationItemProps>;
   brandDesc?: string;
   children?: JSX.Element;
@@ -18,25 +20,226 @@ export interface SidebarProps {
   title?: string;
 }
 
-/**
- * @experimental This component is still in development
- */
+const StyledSidebar = styled.aside`
+  ${({ theme }: { theme: Theme }) => css`
+    aside.dot-sidebar {
+      align-items: stretch;
+      background: ${theme.palette.grey[50]};
+      border-right: 1px solid ${theme.palette.grey[100]};
+      color: ${theme.palette.grey[700]};
+      display: flex;
+      height: 100%;
+      flex-direction: column;
+      justify-content: space-between;
+      letter-spacing: 0.01em;
+      padding-top: ${theme.spacing(1) * 0.5}px;
+      width: 240px;
+      @include transition($prop: width, $func: cubic-bezier(0.4, 0, 0.6, 1));
+
+      h4 {
+        border-bottom: 1px solid ${theme.palette.grey[100]};
+        font-size: 14px;
+        overflow: hidden;
+        padding: ${theme.spacing(1) * 2}px;
+        white-space: nowrap;
+      }
+
+      nav.go-back {
+        border-bottom: 1px solid ${theme.palette.grey[200]};
+
+        li a {
+          color: ${theme.palette.grey[700]};
+          font-size: 16px;
+          padding: ${theme.spacing(1)}px;
+        }
+      }
+
+      nav.side-nav {
+        flex-grow: 2;
+        overflow-x: hidden;
+        overflow-y: scroll;
+
+        li {
+          flex-grow: 2;
+
+          &.divider {
+            border-bottom: 1px solid ${theme.palette.grey[100]};
+            margin: ${theme.spacing(1, 0, 0)}px;
+
+            h5 {
+              font-size: 12px;
+              font-weight: normal;
+              line-height: 36px;
+              margin: 0;
+              padding: ${theme.spacing(0, 1 * 2)}px;
+            }
+          }
+
+          &.has-subnav,
+          a {
+            align-items: stretch;
+            font-size: 14px;
+            height: 44px;
+            padding: 6px;
+
+            &.active,
+            &:hover,
+            &:focus {
+              background: ${theme.palette.grey[200]};
+              color: ${theme.palette.grey[700]};
+            }
+
+            span:not(.dot-icon) {
+              flex-grow: 1;
+            }
+
+            .dot-icon {
+              padding: 0;
+
+              &.first {
+                margin-right: ${theme.spacing(3)}px;
+                padding-left: 10px;
+              }
+            }
+          }
+        }
+      }
+
+      .toggle-nav {
+        border-top: 1px solid ${theme.palette.grey[100]};
+        padding: ${theme.spacing(1)}px 6px 0;
+        text-align: right;
+
+        .dot-icon {
+          transform: rotate(0deg);
+          @include transition($func: cubic-bezier(0.4, 0, 0.2, 1));
+        }
+      }
+
+      .powered-by {
+        border-top: 1px solid ${theme.palette.grey[100]};
+        color: ${theme.palette.grey[400]};
+        display: flex;
+        flex-direction: column;
+        flex-shrink: 0;
+        font-size: 12px;
+        margin: ${theme.spacing(1)}px;
+        overflow: hidden;
+        padding: ${theme.spacing(1)}px;
+
+        span.desc {
+          white-space: nowrap;
+        }
+
+        .company-name {
+          margin-top: ${theme.spacing(1)}px;
+        }
+
+        .d-icon {
+          display: none;
+        }
+      }
+
+      // Styles applied when primary nav is collapsed
+      &.collapsed {
+        width: 56px;
+        @include transition($func: cubic-bezier(0.4, 0, 0.6, 1));
+
+        h4::first-letter {
+          background: ${theme.palette.grey[200]};
+          border-radius: 50%;
+          margin-right: 9999px;
+          padding: ${theme.spacing(1 * 0.5, 1)}px;
+        }
+
+        nav li {
+          &.has-subnav,
+          a {
+            height: 44px;
+            padding: 6px;
+          }
+
+          &.divider h5 {
+            opacity: 0;
+          }
+        }
+
+        .toggle-nav {
+          align-self: center;
+
+          .dot-icon {
+            transform: rotate(180deg);
+            @include transition($func: cubic-bezier(0.4, 0, 0.2, 1));
+          }
+        }
+
+        .powered-by {
+          align-items: center;
+
+          .company-name,
+          span.desc {
+            display: none;
+          }
+
+          .d-icon {
+            display: block;
+          }
+        }
+      }
+    }
+
+    .dot-flyout {
+      margin: 10px 0 0 190px;
+      width: 248px;
+
+      &.collapsed {
+        margin-left: 36px;
+      }
+
+      li {
+        &.active,
+        &:hover,
+        &:focus {
+          background: ${theme.palette.grey[100]};
+        }
+      }
+
+      a {
+        align-items: center;
+        color: ${theme.palette.grey[700]};
+        display: flex;
+        &.active,
+        &:hover,
+        &:focus {
+          background: transparent;
+        }
+      }
+    }
+  `}
+`;
+/** This is a custom component which is used for the sidebar */
 export const DotSidebar = ({
   backItem = [],
   brandDesc,
   children = null,
+  className,
   collapsable = false,
+  'data-testid': dataTestId,
   displayBrand = true,
   goBack,
   navItems = [],
   title,
 }: SidebarProps) => {
   const [open, updateOpen] = useState(true);
+  const rootClasses = useStylesWithRootClass(
+    'dot-sidebar',
+    `${!open ? 'collapsed' : 'expanded'} ${className}`
+  );
 
   return (
-    <aside
-      className={`dot-sidebar ${!open ? 'collapsed' : 'expanded'}`}
-      data-testid="primaryNav"
+    <StyledSidebar
+      className={rootClasses}
+      data-testid={`primaryNav ${dataTestId}`}
     >
       {title && <h4>{title}</h4>}
       {goBack && (
@@ -71,6 +274,6 @@ export const DotSidebar = ({
           <LogoD className="d-icon" title="digital.ai" />
         </div>
       )}
-    </aside>
+    </StyledSidebar>
   );
 };
