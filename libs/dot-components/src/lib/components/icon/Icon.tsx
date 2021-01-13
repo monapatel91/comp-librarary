1cb0eb03f3179cc66bbfbc032598946cadca897a
Matt Haff <mhaff@collab.net>
2021-01-13 14:34:38 -0500

    issue #116: update Icon to be styled component

--- libs/dot-components/src/lib/components/icon/Icon.tsx
+++ libs/dot-components/src/lib/components/icon/Icon.tsx
@@ -8,9 +8,16 @@ import '../../fonts/font-icon/style.scss';
 export type IconFontSize = 'inherit' | 'default' | 'small';
 
 const StyledIcon = styled(Icon)`
+<<<<<<< HEAD
   ${({ theme }: { theme: Theme }) => css`
     &.dot-icon {
       align-items: center;
+=======
+  ${({ theme, iconBgColor }: { theme: Theme; iconBgColor: string }) => css`
+    &.MuiIcon-root {
+      align-items: center;
+      background-color: ${iconBgColor ? iconBgColor : theme.palette.grey[100]}
+>>>>>>> issue #116: update Icon to be styled component
       box-sizing: content-box;
       display: flex;
       font-size: 18px;
@@ -29,6 +36,19 @@ const StyledIcon = styled(Icon)`
         height: 20px;
         width: 20px;
       }
+<<<<<<< HEAD
+=======
+
+      &.transparent {
+        background: transparent;
+      }
+      &.circle {
+        border-radius: 50%;
+      }
+      &.square {
+        border-radius: 4px;
+      }
+>>>>>>> issue #116: update Icon to be styled component
     }
   `}
 `;
@@ -37,7 +57,15 @@ export interface IconProps extends CommonProps {
   /** Determines the size of the icon and spacing around it */
   fontSize?: IconFontSize;
   /** The ID of the icon to display on the button */
+<<<<<<< HEAD
   iconId: string;
+=======
+  icon: string;
+  /** The background color of the icon container */
+  iconBgColor?: string;
+  /** Determines the background color and shape of containing element */
+  iconType?: IconType;
+>>>>>>> issue #116: update Icon to be styled component
   /** Tooltip text displayed on hover */
   title?: string;
 }
@@ -45,12 +73,25 @@ export interface IconProps extends CommonProps {
 /** This component wraps the Icon component from @material-ui. */
 export const DotIcon = ({
   className,
+<<<<<<< HEAD
   'data-testid': dataTestId,
   fontSize = 'default',
   iconId,
   title = '',
 }: IconProps) => {
   const rootClasses = useStylesWithRootClass('dot-icon', className);
+=======
+  'data-testid': dataTestId = 'icon',
+  fontSize = 'default',
+  icon,
+  iconType = 'transparent',
+  title = '',
+}: IconProps) => {
+  const rootClasses = useStylesWithRootClass(
+    'dot-icon',
+    `${iconType} ${className}`
+  );
+>>>>>>> issue #116: update Icon to be styled component
 
   return (
     <StyledIcon
@@ -60,7 +101,11 @@ export const DotIcon = ({
       fontSize={fontSize}
       title={title}
     >
+<<<<<<< HEAD
       <i className={`icon-${iconId}`} />
+=======
+      <i className={`icon-${icon}`} />
+>>>>>>> issue #116: update Icon to be styled component
     </StyledIcon>
   );
 };
