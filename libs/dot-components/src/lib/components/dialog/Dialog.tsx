ee4203b28f3819c67fee3949ff609de6df129ad2
Josh Schoen <jschoen@digital.ai>
2021-01-15 15:29:24 -0600

    issue: 118 made dialog children required

--- libs/dot-components/src/lib/components/dialog/Dialog.tsx
+++ libs/dot-components/src/lib/components/dialog/Dialog.tsx
@@ -15,7 +15,7 @@ export interface DialogButtonProps {
   /** If true, the button will be disabled. */
   disabled?: boolean;
   /** The text displayed on the button */
-  children?: string;
+  children: string;
   /** The icon to display on the button */
   iconId?: string;
   /** The type of button to be used */
