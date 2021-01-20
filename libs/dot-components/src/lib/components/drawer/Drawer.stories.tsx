1f48e4f7ae9c4260137207db81c0ae49514f53f7
Josh Schoen <jschoen@digital.ai>
2021-01-20 11:10:22 -0600

    issue: #112 added styled components css

--- libs/dot-components/src/lib/components/drawer/Drawer.stories.tsx
+++ libs/dot-components/src/lib/components/drawer/Drawer.stories.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { useArgs } from '@storybook/client-api';
 
 import { DotDrawer, DrawerProps } from './Drawer';
+import DotButton from '../button/Button';
+import { on } from 'process';
 
 export default {
   title: 'Experimental/Drawer',
@@ -9,7 +12,26 @@ export default {
   argTypes: {
     open: { defaultValue: true },
     children: { defaultValue: 'I am Batman' },
+    width: { defaultValue: '256px' }
   },
 } as Meta;
 
-export const Default: Story<DrawerProps> = (args) => <DotDrawer {...args} />;
+
+
+export const Default: Story<DrawerProps> = (args) => { 
+const [_args, updateArgs] = useArgs();
+const onClose = () => {
+  _args.open = !_args.open;
+  updateArgs(_args);
+}
+return (
+  <>
+  <DotButton onClick={onClose}>Toggle Drawer</DotButton>
+  <DotDrawer onClose={onClose}  {...args}>
+  <DotButton onClick={onClose}>Toggle Drawer</DotButton>
+</DotDrawer>
+</>
+)
+
+
+};
--- libs/dot-components/src/lib/components/drawer/Drawer.tsx
+++ libs/dot-components/src/lib/components/drawer/Drawer.tsx
@@ -1,47 +1,72 @@
 import React, { MouseEvent } from 'react';
-import { Drawer } from '@material-ui/core';
-import './Drawer.scss';
+import { Drawer, Theme } from '@material-ui/core';
+import { CommonProps } from '../CommonProps';
+import styled, { css } from 'styled-components';
+// import './Drawer.scss';
 
 export type DrawerAnchor = 'bottom' | 'left' | 'right' | 'top';
 export type DrawerVariant = 'permanent' | 'persistent' | 'temporary';
 
-export interface DrawerProps {
+export interface DrawerProps extends CommonProps {
   /** Side from which the drawer will appear 'bottom', 'left', 'right', 'top' */
   anchor?: DrawerAnchor;
   /** string or JSX element that is displayed inside the drawer */
   children?: string | JSX.Element | JSX.Element[];
-  /** space delimited set of classes applied to the root component */
-  classes?: string;
   /** Callback fired when the component requests to be closed. */
   onClose?: (event: MouseEvent | KeyboardEvent) => void;
   /** If true, the drawer is open. */
   open: boolean;
   /** The variant to use 'permanent', 'persistent', 'temporary' */
   variant?: DrawerVariant;
+  /** The width of the drawer defaults to 256px */
+  width?: string;
 }
+
+const setDrawerPosition = (anchor): string => {
+  let left = 'auto';
+  if (anchor === 'bottom' || anchor === 'top') {
+    left = '50%';
+  }
+  return `transform: translate(${left === 'auto' ? 0 : `-${left}`}, 0); left: ${left}`;
+}
+
+const StyledDrawer = styled(Drawer)<DrawerProps>`
+  ${({ theme }: { theme: Theme }) => css`
+    .dot-drawer-paper {
+      width: ${({width}: DrawerProps ) => width};
+      padding: ${theme.spacing(2)}px;
+      ${({ anchor }: DrawerProps) => setDrawerPosition(anchor)};
+    }
+  `}
+`;
 
 /**
  * @experimental This component is still in development
  */
 export const DotDrawer = ({
   anchor = 'right',
+  className,
   children,
-  classes,
+  'data-testid': dataTestId,
   onClose,
   open,
   variant = 'temporary',
+  width = '256px'
 }: DrawerProps) => {
   return (
-    <Drawer
+    <StyledDrawer
+      data-testid={dataTestId}
       anchor={anchor}
       classes={{ paper: 'dot-drawer-paper' }}
-      className={`dot-drawer ${classes}`}
+      className={`dot-drawer ${className}`}
       onClose={(event: MouseEvent) => onClose && onClose(event)}
       open={open}
+      PaperProps={{['data-testid']: 'drawer-paper'} as any}
       variant={variant}
+      width={width}
     >
       {children}
-    </Drawer>
+    </StyledDrawer>
   );
 };
 
