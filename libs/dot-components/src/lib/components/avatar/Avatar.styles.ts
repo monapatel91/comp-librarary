4e05c9d100684df26032a80467376ff8f70b3f49
Stephen Elsemore <root@ctf-dev-build-box.collab.net>
2021-01-28 13:42:23 +0100

    issue #163: move Avatar styles to separate file

---
+++ libs/dot-components/src/lib/components/avatar/Avatar.styles.ts
@@ -0,0 +1,34 @@
+import { Avatar, Theme } from '@material-ui/core';
+import styled, { css } from 'styled-components';
+
+export const rootClassName = 'dot-avatar';
+
+const avatarSpacing = {
+  small: 3,
+  medium: 5,
+  large: 7,
+};
+
+export const StyledAvatar = styled(Avatar)`
+  ${({ theme }: { theme: Theme }) => css`
+    &.MuiAvatar-root {
+      background-color: ${theme.palette.grey[100]};
+      color: ${theme.palette.text.primary};
+
+      &.small {
+        height: ${theme.spacing(avatarSpacing.small)}px;
+        width: ${theme.spacing(avatarSpacing.small)}px;
+      }
+
+      &.medium {
+        height: ${theme.spacing(avatarSpacing.medium)}px;
+        width: ${theme.spacing(avatarSpacing.medium)}px;
+      }
+
+      &.large {
+        height: ${theme.spacing(avatarSpacing.large)}px;
+        width: ${theme.spacing(avatarSpacing.large)}px;
+      }
+    }
+  `}
+`;
