bc2429d7e59b8f7983bcd5d115c008aa6f57a96a
Stephen Elsemore <root@ctf-dev-build-box.collab.net>
2021-01-28 12:50:01 +0100

    issue #163: fix StyledToolbar export

--- libs/dot-components/src/lib/components/action-toolbar/ActionToolbar.styles.ts
+++ libs/dot-components/src/lib/components/action-toolbar/ActionToolbar.styles.ts
@@ -1,11 +1,10 @@
 import styled, { css } from 'styled-components';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Theme } from '@material-ui/core/styles';
-import { DotActionBarProps } from './ActionToolbar.propTypes';
 
 export const rootClassName = 'dot-action-toolbar';
 
-export const StyledToolbar = styled(Toolbar)<DotActionBarProps>`
+export const StyledToolbar = styled(Toolbar)`
   ${({ theme }: { theme: Theme }) => css`
     &.${rootClassName} {
       border-bottom: 1px solid ${theme.palette.grey[100]};
