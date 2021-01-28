c18e9e1b044a9f2c7ded18c36cb862f44777249b
Stephen Elsemore <root@ctf-dev-build-box.collab.net>
2021-01-28 11:43:58 +0100

    issue #163: move ActionToolbar props and styles to separate files

---
+++ libs/dot-components/src/lib/components/action-toolbar/ActionToolbar.propTypes.ts
@@ -0,0 +1,10 @@
+import { CommonProps } from '../CommonProps';
+
+export type DotActionBarVarient = 'regular' | 'dense';
+
+export interface DotActionBarProps extends CommonProps {
+    /** string or JSX element that is displayed inside the toolbar */
+    children?: string | JSX.Element | JSX.Element[];
+    /** DotActionBarVarient dense and regular for toolbar height */
+    variant?: DotActionBarVarient;
+}
--- libs/dot-components/src/lib/components/action-toolbar/ActionToolbar.tsx
+++ libs/dot-components/src/lib/components/action-toolbar/ActionToolbar.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import Toolbar from '@material-ui/core/Toolbar';
-import { createStyles, Theme, StyleRules } from '@material-ui/core/styles';
 import { useStylesWithRootClass } from '../makeStylesWithRootClass';
-import { CommonProps } from '../CommonProps';
-import styled, { css } from 'styled-components';
-
-export type DotActionBarVarient = 'regular' | 'dense';
-
-export interface DotActionBarProps extends CommonProps {
-  /** string or JSX element that is displayed inside the toolbar */
-  children?: string | JSX.Element | JSX.Element[];
-  /** DotActionBarVarient dense and regular for toolbar height */
-  variant?: DotActionBarVarient;
-}
-
-const StyledToolbar = styled(Toolbar)<DotActionBarProps>`
-  ${({ theme }: { theme: Theme }) => css`
-    &.dot-action-toolbar {
-      border-bottom: 1px solid ${theme.palette.grey[100]};
-    }
-  `}
-`;
+import { DotActionBarProps } from './ActionToolbar.propTypes'
+import { rootClassName, StyledToolbar } from './ActionToolbar.styles';
 
 export function DotActionToolbar({
   children,
   variant = 'dense',
 }: DotActionBarProps) {
-  const rootClasses = useStylesWithRootClass('dot-action-toolbar');
+  const rootClasses = useStylesWithRootClass(rootClassName);
 
   return (
     <StyledToolbar className={rootClasses} variant={variant}>
