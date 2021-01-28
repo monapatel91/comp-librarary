15ad16d9f0ce6815bfbde3f064cd685349fb01fe
Matt Haff <mhaff@collab.net>
2021-01-28 11:54:22 -0500

    issue #166: rebase against master

--- libs/dot-components/src/lib/components/empty-state/EmptyState.styles.ts
+++ libs/dot-components/src/lib/components/empty-state/EmptyState.styles.ts
@@ -1,15 +1,27 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { Theme } from '@material-ui/core';
 
 export const rootClassName = 'dot-empty-state';
 
 export const StyledEmptyState = styled.div`
-  &.dot-empty-state {
-    margin: 0 auto;
-    max-width: 600px;
-    text-align: center;
+  ${({ theme }: { theme: Theme }) => css`
+    &.dot-empty-state {
+      margin: 0 auto;
+      max-width: 600px;
+      text-align: center;
 
-    .empty-state-image {
-      min-height: 239px;
+      .empty-state-image {
+        min-height: 239px;
+        margin-bottom: ${theme.spacing(5)}px;
+      }
+
+      h2 {
+        margin-bottom: ${theme.spacing(1)}px;
+      }
+
+      .dot-button {
+        margin-top: ${theme.spacing(4)}px;
+      }
     }
-  }
+  `}
 `;
