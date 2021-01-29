5b7a17cfbda65d9651ec3ed4e1af21b12e14e8e4
Josh Schoen <jschoen@digital.ai>
2021-01-29 11:43:31 -0600

    issue #107 and #108 fixed theme variables

--- libs/dot-components/src/lib/components/input-form-fields/InputFormFields.styles.ts
+++ libs/dot-components/src/lib/components/input-form-fields/InputFormFields.styles.ts
@@ -40,7 +40,7 @@ export const StyledTextField = styled(TextField)`
         }
       }
       .MuiSelect-icon {
-        right: ${InputProps.endAdornment ? '44px' : '12px'};
+        right: ${InputProps.endAdornment ? `${theme.spacing(5.5)}px` : `${theme.spacing(1.5)}px`};
       }
       &.${warningClassName} {
         .MuiOutlinedInput-notchedOutline {
