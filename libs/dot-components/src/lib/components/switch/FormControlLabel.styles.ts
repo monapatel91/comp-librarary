4e7c04aa994809dc7539fe78d9d375f2ba8466a2
Stephen Elsemore <root@ctf-dev-build-box.collab.net>
2021-02-03 15:27:19 +0100

    Issue #110: formatting - clean up whitespace in FormControlLabel styling

--- libs/dot-components/src/lib/components/switch/FormControlLabel.styles.ts
+++ libs/dot-components/src/lib/components/switch/FormControlLabel.styles.ts
@@ -6,10 +6,8 @@ export const rootClassName = 'dot-form-control-label';
 export const StyledFormControlLabel = styled(FormControlLabel)`
   ${({ theme }) => css`{
     &.${rootClassName} {
-
       .MuiTypography-body1 {
         margin-bottom: 0px;
       }
-
   `}
 `;
