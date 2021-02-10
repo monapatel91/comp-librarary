740baa594f51e9eb3d19cfbb6b68e573bb157b08
Stephen Elsemore <root@ctf-dev-build-box.collab.net>
2021-02-10 16:36:41 +0100

    Issue #103: refactor StepCard

--- libs/dot-components/src/lib/components/step-card/StepCard.tsx
+++ libs/dot-components/src/lib/components/step-card/StepCard.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { CardActions } from '@material-ui/core';
+import React, { Fragment, MouseEvent, useState } from 'react';
+import { CardActions, CardHeader, Menu, MenuItem } from '@material-ui/core';
 import { CommonProps } from '../CommonProps';
 import { useStylesWithRootClass } from '../useStylesWithRootClass';
-import { rootClassName, StyledStepCard } from './StepCard.styles';
+import { rootClassName } from './StepCard.styles';
 import { DotIcon } from '../icon/Icon';
+import { DotIconButton } from '../button/IconButton';
 import { DotAvatar } from '../avatar/Avatar';
-import { CardMenuOption } from '../card/Card';
+import { DotCard, CardMenuOption } from '../card/Card';
 
 export interface TeamObject {
   name: string;
@@ -47,32 +48,79 @@ export const DotStepCard = ({
 }: StepCardProps) => {
   const rootClasses = useStylesWithRootClass(rootClassName, className);
   const preHeader = <DotIcon fontSize="small" iconId={stepIcon} />;
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = (action: () => void) => {
+    setAnchorEl(null);
+    setOpen(false);
+    action();
+  };
+
+  const headerAction =
+    menuOptions.length > 0 ? (
+      <Fragment>
+        <DotIconButton
+          className="expand-button"
+          data-testid="card-header-action-button"
+          iconId="options"
+          onClick={handleClick}
+          size="small"
+        />
+        <Menu anchorEl={anchorEl} keepMounted open={open} onClose={handleClose}>
+          {menuOptions.map((option: CardMenuOption, index: number) => (
+            <MenuItem
+              key={index}
+              onClick={() => handleClose(option.action)}
+              data-testid="card-header-action-menu-option"
+            >
+              {option.displayText}
+            </MenuItem>
+          ))}
+        </Menu>
+      </Fragment>
+    ) : null;
 
   return (
-    <StyledStepCard
+    <DotCard
       className={rootClasses}
       data-testid={dataTestId}
-      menuOptions={menuOptions}
-      preHeader={preHeader}
-      subheader={subheader}
-      title={title}
     >
-      {(team || user) && (
-        <CardActions
-          className="dot-card-actions"
-          data-testid="step-card-actions"
-        >
-          {team && team.avatar && (
-            <div className="team">
-              <DotAvatar alt={team.name} className="team-avatar" size="small" />
-            </div>
-          )}
-          {user && (
-            <DotAvatar alt={user.name} className="user-avatar" size="small" />
-          )}
-        </CardActions>
-      )}
-    </StyledStepCard>
+      <Fragment>
+        {preHeader}
+        <CardHeader
+          action={headerAction}
+          subheader={subheader}
+          subheaderTypographyProps={{ variant: 'body2' }}
+          title={title}
+          titleTypographyProps={{ variant: 'h4' }}
+        />
+        {(team || user) && (
+          <CardActions
+            className="dot-card-actions"
+            data-testid="step-card-actions"
+          >
+            {team && team.avatar && (
+              <div className="team">
+                <DotAvatar
+                  alt={team.name}
+                  className="team-avatar"
+                  size="small"
+                />
+              </div>
+            )}
+            {user && (
+              <DotAvatar alt={user.name} className="user-avatar" size="small" />
+            )}
+          </CardActions>
+        )}
+      </Fragment>
+    </DotCard>
   );
 };
 
