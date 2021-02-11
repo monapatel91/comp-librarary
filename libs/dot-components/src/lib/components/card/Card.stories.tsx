8901f6ed90c4f06d243efa726035742ffb6d34be
Stephen Elsemore <root@ctf-dev-build-box.collab.net>
2021-02-11 18:13:40 +0100

    Issue #103: additional storybook Card stories

--- libs/dot-components/src/lib/components/card/Card.stories.tsx
+++ libs/dot-components/src/lib/components/card/Card.stories.tsx
@@ -1,43 +1,153 @@
 import React, { Fragment } from 'react';
-import { Typography } from '@material-ui/core';
-import { Meta } from '@storybook/react/types-6-0';
+import { Typography, CardHeader, CardMedia } from '@material-ui/core';
+import { Story, Meta } from '@storybook/react/types-6-0';
 
-import { DotCard } from './Card';
+import { DotCard, CardProps } from './Card';
 import { DotCardHeader } from './CardHeader';
 import { DotCardContent } from './CardContent';
 import { DotCardFooter } from './CardFooter';
+import DotAvatar from '../avatar/Avatar';
+import DotIconButton from '../button/IconButton';
+import imageFile from '../../assets/logo_digital_ai.svg';
+import DotButton from '../button/Button';
 
-const DotCardTemplate = ({ cardHeader, cardContent, cardFooter, ...args }) => (
-  <DotCard>
-    <Fragment>
-      <DotCardHeader {...cardHeader} />
-      <DotCardContent {...cardContent}>
-        <Typography variant="body1">{cardContent}</Typography>
-      </DotCardContent>
-      <DotCardFooter {...cardFooter}>
-        <Typography variant="body1">{cardFooter}</Typography>
-      </DotCardFooter>
-    </Fragment>
-  </DotCard>
+const headerOnly = 'Header only';
+const headerAndContent = 'Header and content';
+const headerContentAndFooter = 'Header, content and footer';
+const childrenOptions = [headerOnly, headerAndContent, headerContentAndFooter];
+
+const avatar = <DotAvatar text="SE" alt="Chef"></DotAvatar>;
+
+const defaultHeader = (
+  <DotCardHeader
+    title="Hello World"
+    subheader="Well hello there"
+  ></DotCardHeader>
+);
+
+const complexHeaderAction = (
+  <DotIconButton
+    className="expand-button"
+    data-testid="card-header-action-button"
+    iconId="options"
+    size="small"
+  />
+);
+
+const complexHeader = (
+  <CardHeader
+    avatar={avatar}
+    action={complexHeaderAction}
+    title="digital.ai"
+    titleTypographyProps={{ variant: 'h4' }}
+    subheader="We make digital transformation deliver business value."
+    subheaderTypographyProps={{ variant: 'body2' }}
+  ></CardHeader>
+);
+
+const defaultContent = (
+  <DotCardContent>
+    <Typography variant="body1">Do you come to this card often?</Typography>
+  </DotCardContent>
+);
+
+const complexContent = (
+  <Fragment>
+    <CardMedia image={imageFile} style={{ height: '200px' }}></CardMedia>
+    <DotCardContent>
+      <Typography variant="body1">
+        Finally, the intelligence, agility, and security you need to create
+        innovative digital experiences customers trust.
+      </Typography>
+    </DotCardContent>
+  </Fragment>
+);
+
+const defaultFooter = (
+  <DotCardFooter>
+    <Typography variant="body1">This is a footer</Typography>
+  </DotCardFooter>
+);
+
+const complexFooter = (
+  <DotCardFooter>
+    <DotButton type="text">Learn more</DotButton>
+  </DotCardFooter>
+);
+
+const defaultHeaderAndContentChildren = (
+  <Fragment>
+    {defaultHeader}
+    {defaultContent}
+  </Fragment>
+);
+
+const complexHeaderAndContentChildren = (
+  <Fragment>
+    {complexHeader}
+    {complexContent}
+  </Fragment>
+);
+
+const defaultHeaderContentAndFooterChildren = (
+  <Fragment>
+    {defaultHeader}
+    {defaultContent}
+    {defaultFooter}
+  </Fragment>
+);
+
+const complexHeaderContentAndFooterChildren = (
+  <Fragment>
+    {complexHeader}
+    {complexContent}
+    {complexFooter}
+  </Fragment>
 );
 
 export default {
   title: 'Components/Card',
   component: DotCard,
+  subcomponents: { DotCardHeader, DotCardContent, DotCardFooter },
   argTypes: {
     children: {
       control: {
-        type: 'text',
-        value:
-          'Set properties for DotCardHeader, DotCardContent and DotCardFooter children using the controls below.',
+        type: 'select',
+        options: childrenOptions,
+        default: headerContentAndFooter,
       },
     },
   },
 } as Meta;
 
-const header = { title: 'Hello World', subheader: 'Well hello there' };
-const content = 'Do you come to this card often?';
-const footer = 'This is a footer';
+export const Default: Story<CardProps> = (args) => {
+  const { children } = args;
+  let selectedChildren;
+  switch (children) {
+    case headerOnly:
+      selectedChildren = defaultHeader;
+      break;
+    case headerAndContent:
+      selectedChildren = defaultHeaderAndContentChildren;
+      break;
+    default:
+      selectedChildren = defaultHeaderContentAndFooterChildren;
+  }
+  return <DotCard {...args} children={selectedChildren} />;
+};
 
-export const Default = DotCardTemplate.bind({});
-Default.args = { cardHeader: header, cardContent: content, cardFooter: footer };
+export const ComplexCardWithMedia: Story<CardProps> = (args) => {
+  const { children } = args;
+  let selectedChildren;
+  switch (children) {
+    case headerOnly:
+      selectedChildren = complexHeader;
+      break;
+    case headerAndContent:
+      selectedChildren = complexHeaderAndContentChildren;
+      break;
+    default:
+      selectedChildren = complexHeaderContentAndFooterChildren;
+  }
+  return <DotCard {...args} children={selectedChildren} />;
+};
