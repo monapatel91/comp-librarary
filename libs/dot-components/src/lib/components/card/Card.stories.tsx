2734d4a5a9f7ad162d644ee163ea9b9f7f36fcc0
Stephen Elsemore <root@ctf-dev-build-box.collab.net>
2021-02-11 12:36:16 +0100

    Issue #103: improved storybook experience (maybe)

--- libs/dot-components/src/lib/components/card/Card.stories.tsx
+++ libs/dot-components/src/lib/components/card/Card.stories.tsx
@@ -1,37 +1,43 @@
 import React, { Fragment } from 'react';
 import { Typography } from '@material-ui/core';
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Meta } from '@storybook/react/types-6-0';
 
-import { DotCard, CardProps } from './Card';
+import { DotCard } from './Card';
 import { DotCardHeader } from './CardHeader';
 import { DotCardContent } from './CardContent';
 import { DotCardFooter } from './CardFooter';
+
+const DotCardTemplate = ({ cardHeader, cardContent, cardFooter, ...args }) => (
+  <DotCard>
+    <Fragment>
+      <DotCardHeader {...cardHeader} />
+      <DotCardContent {...cardContent}>
+        <Typography variant="body1">{cardContent}</Typography>
+      </DotCardContent>
+      <DotCardFooter {...cardFooter}>
+        <Typography variant="body1">{cardFooter}</Typography>
+      </DotCardFooter>
+    </Fragment>
+  </DotCard>
+);
 
 export default {
   title: 'Experimental/Card',
   component: DotCard,
   argTypes: {
     children: {
-      defaultValue: (
-        <Fragment>
-          <DotCardHeader
-            title="Hello World"
-            subheader="Well hello there"
-          ></DotCardHeader>
-          <DotCardContent>
-            <Typography variant="body1">
-              Do you come to this card often?
-            </Typography>
-          </DotCardContent>
-          <DotCardFooter>
-            <Typography variant="body1">This is a footer</Typography>
-          </DotCardFooter>
-        </Fragment>
-      ),
+      control: {
+        type: 'text',
+        value:
+          'Set properties for DotCardHeader, DotCardContent and DotCardFooter children using the controls below.',
+      },
     },
   },
 } as Meta;
 
-export const Default: Story<CardProps> = (args) => {
-  return <DotCard {...args} />;
-};
+const header = { title: 'Hello World', subheader: 'Well hello there' };
+const content = 'Do you come to this card often?';
+const footer = 'This is a footer';
+
+export const Default = DotCardTemplate.bind({});
+Default.args = { cardHeader: header, cardContent: content, cardFooter: footer };
