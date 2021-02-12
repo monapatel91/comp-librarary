f5b7e797c3a8e03764e35fbe732d51e136403360
Matt Haff <mhaff@collab.net>
2021-02-12 12:20:34 -0500

    issue #198: fix failing test due to import

--- libs/dot-components/src/lib/ctm/progression-board/ProgressionBoard.spec.tsx
+++ libs/dot-components/src/lib/ctm/progression-board/ProgressionBoard.spec.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { renderWithTheme as render } from '../../testing-utils/RenderWithTheme';
-import { ProgressionBoard } from './ProgressionBoard';
+import { DotProgressionBoard } from './ProgressionBoard';
 import samplePhases from './sampleData';
 
 describe('ProgressionBoard', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<ProgressionBoard phases={samplePhases} />);
+    const { baseElement } = render(
+      <DotProgressionBoard phases={samplePhases} />
+    );
     expect(baseElement).toBeTruthy();
+  });
+
+  xit('should have five columns', () => {
+    // tests go here
+  });
+
+  xit('should have three rows', () => {
+    // tests go here
+  });
+
+  xit('should have cards in appropriate columns', () => {
+    // tests go here
+  });
+
+  xit('cards should have correct information', () => {
+    // tests go here
   });
 });
