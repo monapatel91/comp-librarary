838bdb52da73daeb6f6348979bd0f79935eaac99
Matt Haff <mhaff@collab.net>
2021-02-17 11:40:57 -0500

    issue #198: expand unit and e2e tests

--- libs/dot-components/src/lib/ctm/progression-board/ProgressionBoard.spec.tsx
+++ libs/dot-components/src/lib/ctm/progression-board/ProgressionBoard.spec.tsx
@@ -27,18 +27,31 @@ describe('ProgressionBoard', () => {
     expect(cols).toHaveLength(15);
   });
 
-  it('should have correct number of cards in appropriate columns', () => {
+  it('should have correct number of cards', () => {
     render(<DotProgressionBoard phases={samplePhases} />);
     const cards = screen.getAllByTestId('card');
 
     expect(cards).toHaveLength(10);
   });
 
-  xit('cards should have correct information', () => {
+  it('cards should have correct information', () => {
     render(<DotProgressionBoard phases={samplePhases} />);
     const cards = screen.getAllByTestId('card');
+    const links = screen.getAllByRole('link');
+    const list = screen.getAllByRole('list');
 
-    screen.debug(cards[0]);
-    // expect(cards[0]);
+    expect(links[0]).toContainHTML('3.1.83 - 3.1.100');
+    expect(links[2]).toContainHTML('3.1');
+    expect(links[3]).toContainHTML('83 - 100');
+
+    expect(cards[0]).toContainHTML(`<div class="actions-container">`);
+
+    expect(cards[0]).toContainHTML(`<i class="icon-error-outlines">`);
+    expect(cards[0]).toContainHTML(`<i class="icon-file-dotted">`);
+    expect(cards[0]).toContainHTML(`<i class="icon-check-solid">`);
+    expect(cards[0]).toContainHTML(`<i class="icon-rogue-commits">`);
+
+    expect(list[1]).toContainHTML(`<li class="maintain">`);
+    expect(list[1]).toContainHTML(`<li class="improve">`);
   });
 });
