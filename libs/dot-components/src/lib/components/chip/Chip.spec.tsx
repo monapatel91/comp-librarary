81cdc1f876281371e53b79b53c30606405a41518
Stephen Elsemore <root@ctf-dev-build-box.collab.net>
2021-02-18 14:49:10 +0100

    Issue #106: 'beforeEach' to 'before' in e2e and cleanup unit tests

--- libs/dot-components/src/lib/components/chip/Chip.spec.tsx
+++ libs/dot-components/src/lib/components/chip/Chip.spec.tsx
@@ -7,81 +7,79 @@ import DotAvatar from '../avatar/Avatar';
 import { DotIcon } from '../icon/Icon';
 
 describe('DotChip', () => {
-  describe('Props', () => {
-    const avatar = (
-      <DotAvatar alt="Batman" size="small" data-testid="test-avatar" />
+  const avatar = (
+    <DotAvatar alt="Batman" size="small" data-testid="test-avatar" />
+  );
+  const startIcon = <DotIcon iconId="home" data-testid="test-icon" />;
+
+  it('should render with all props', () => {
+    render(
+      <DotChip
+        avatar={avatar}
+        className="custom-test-class"
+        data-testid="test-chip"
+        disabled
+        error
+        size="small"
+      >
+        Hello
+      </DotChip>
     );
-    const startIcon = <DotIcon iconId="home" data-testid="test-icon" />;
+    const avatarTestId = screen.getByTestId('test-avatar');
+    const chipTestId = screen.getByTestId('test-chip');
+    expect(avatarTestId).toBeVisible();
+    expect(chipTestId).toHaveClass('custom-test-class');
+    expect(chipTestId).toHaveClass('Mui-disabled');
+    expect(chipTestId).toHaveClass('Mui-error');
+    expect(chipTestId).toHaveClass('MuiChip-sizeSmall');
+  });
 
-    it('should render with all props', () => {
-      render(
-        <DotChip
-          avatar={avatar}
-          className="custom-test-class"
-          data-testid="test-chip"
-          disabled
-          error
-          size="small"
-        >
-          Hello
-        </DotChip>
-      );
-      const avatarTestId = screen.getByTestId('test-avatar');
-      const chipTestId = screen.getByTestId('test-chip');
-      expect(avatarTestId).toBeVisible();
-      expect(chipTestId).toHaveClass('custom-test-class');
-      expect(chipTestId).toHaveClass('Mui-disabled');
-      expect(chipTestId).toHaveClass('Mui-error');
-      expect(chipTestId).toHaveClass('MuiChip-sizeSmall');
-    });
+  it('should give avatar precedence over startIcon', () => {
+    const { baseElement } = render(
+      <DotChip avatar={avatar} startIcon={startIcon}>
+        Hello
+      </DotChip>
+    );
+    const avatarTestId = screen.getByTestId('test-avatar');
+    expect(avatarTestId).toBeVisible();
+  });
 
-    it('should give avatar precedence over startIcon', () => {
-      const { baseElement } = render(
-        <DotChip avatar={avatar} startIcon={startIcon}>
-          Hello
-        </DotChip>
-      );
-      const avatarTestId = screen.getByTestId('test-avatar');
-      expect(avatarTestId).toBeVisible();
-    });
+  it('should render startIcon if no avatar', () => {
+    render(<DotChip startIcon={startIcon}>Hello</DotChip>);
+    expect(screen.getByTestId('test-icon')).toBeVisible();
+  });
 
-    it('should render startIcon if no avatar', () => {
-      render(<DotChip startIcon={startIcon}>Hello</DotChip>);
-      expect(screen.getByTestId('test-icon')).toBeVisible();
-    });
+  it('should allow the user to click if clickable', () => {
+    const onClick = jest.fn();
+    render(
+      <DotChip isClickable={true} onClick={onClick}>
+        Hello
+      </DotChip>
+    );
 
-    it('should allow the user to click if clickable', () => {
-      const onClick = jest.fn();
-      render(
-        <DotChip isClickable={true} onClick={onClick}>
-          Hello
-        </DotChip>
-      );
+    const chip = screen.getByRole('button');
+    userEvent.click(chip);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
 
-      const chip = screen.getByRole('button');
-      userEvent.click(chip);
-      expect(onClick).toHaveBeenCalledTimes(1);
-    });
+  it('should not display the delete icon if not deletable', () => {
+    const { baseElement } = render(
+      <DotChip isDeletable={false}>Hello</DotChip>
+    );
+    const deleteIcon = baseElement.querySelector('.MuiChip-deleteIcon');
+    expect(deleteIcon).not.toBeTruthy();
+  });
 
-    it('should not display the delete icon if not deletable', () => {
-      const { baseElement } = render(
-        <DotChip isDeletable={false}>Hello</DotChip>
-      );
-      const deleteIcon = baseElement.querySelector('.MuiChip-deleteIcon');
-      expect(deleteIcon).not.toBeTruthy();
-    });
-
-    it('should display the delete icon if deletable', () => {
-      const onDelete = jest.fn();
-      const { baseElement } = render(
-        <DotChip isDeletable={true} onDelete={onDelete}>
-          Hello
-        </DotChip>
-      );
-      const deleteIcon = baseElement.querySelector('.MuiChip-deleteIcon');
-      expect(deleteIcon).toBeTruthy();
-      userEvent.click(deleteIcon);
-      expect(onDelete).toHaveBeenCalledTimes(1);
-    });
+  it('should display the delete icon if deletable', () => {
+    const onDelete = jest.fn();
+    const { baseElement } = render(
+      <DotChip isDeletable={true} onDelete={onDelete}>
+        Hello
+      </DotChip>
+    );
+    const deleteIcon = baseElement.querySelector('.MuiChip-deleteIcon');
+    expect(deleteIcon).toBeTruthy();
+    userEvent.click(deleteIcon);
+    expect(onDelete).toHaveBeenCalledTimes(1);
   });
 });
