4795918ffa8f0348c103fd252ccb400a4b9149e6
Matt Haff <mhaff@collab.net>
2021-02-18 17:23:13 -0500

    issue #43: extending e2e tests

--- apps/dot-components-e2e/src/integration/Card/Card.spec.ts
+++ apps/dot-components-e2e/src/integration/Card/Card.spec.ts
@@ -9,4 +9,16 @@ describe('dot-components: Card component', () => {
     cy.get('h6').should('contain', 'Hello World');
     cy.get('i').should('have.class', 'icon-options');
   });
+
+  describe('style decisions', () => {
+    it('size and spacing is correct', () => {
+      cy.get('div.dot-card')
+        .should('have.css', 'color', 'rgb(102, 115, 133)')
+        .and('have.css', 'font-size', '12px')
+        .and('have.css', 'padding-left', '16px')
+        .and('have.css', 'padding-right', '16px')
+        .and('have.css', 'padding-top', '16px')
+        .and('have.css', 'padding-bottom', '16px');
+    });
+  });
 });
--- apps/dot-components-e2e/src/integration/ConfirmationDialog/ConfirmationDialog.spec.ts
+++ apps/dot-components-e2e/src/integration/ConfirmationDialog/ConfirmationDialog.spec.ts
@@ -10,4 +10,44 @@ describe('dot-components: Confirmation Dialog component', () => {
   it('should render the component', () => {
     cy.get('h2').should('contain', 'Please confirm');
   });
+
+  describe('style decisions', () => {
+    it('backdrop is correct color', () => {
+      cy.get('div.MuiBackdrop-root').should(
+        'have.css',
+        'background-color',
+        'rgba(0, 0, 0, 0.5)'
+      );
+    });
+
+    it('dialog sizing', () => {
+      cy.get('div.MuiDialog-paper')
+        .should('have.css', 'max-width', '600px')
+        .and('have.css', 'margin-left', '32px');
+    });
+
+    it('dialog header sizing', () => {
+      cy.get('div.MuiDialogTitle-root')
+        .should('have.css', 'padding-top', '8px')
+        .and('have.css', 'padding-left', '16px')
+        .and('have.css', 'padding-right', '16px')
+        .and('have.css', 'padding-bottom', '8px');
+    });
+
+    it('dialog content sizing', () => {
+      cy.get('div.dot-dialog-content')
+        .should('have.css', 'padding-top', '8px')
+        .and('have.css', 'padding-left', '16px')
+        .and('have.css', 'padding-right', '16px')
+        .and('have.css', 'padding-bottom', '8px');
+    });
+
+    it('dialog footer sizing', () => {
+      cy.get('div.dot-dialog-actions')
+        .should('have.css', 'padding-top', '8px')
+        .and('have.css', 'padding-left', '8px')
+        .and('have.css', 'padding-right', '8px')
+        .and('have.css', 'padding-bottom', '8px');
+    });
+  });
 });
