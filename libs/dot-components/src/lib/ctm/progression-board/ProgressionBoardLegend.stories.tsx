657eda09020c0e618558e35b4c9fe34905305226
Matt Haff <mhaff@collab.net>
2021-02-19 15:14:38 -0500

    issue #199: cleanup of storybook

--- libs/dot-components/src/lib/ctm/progression-board/ProgressionBoardLegend.stories.tsx
+++ libs/dot-components/src/lib/ctm/progression-board/ProgressionBoardLegend.stories.tsx
@@ -3,16 +3,12 @@ import { Meta, Story } from '@storybook/react/types-6-0';
 
 import {
   DotProgressionBoardLegend,
-  legendItems,
   ProgressionBoardLegendProps,
 } from './ProgressionBoardLegend';
 
 export default {
   title: 'Experimental/ProgressionBoardLegend',
   component: DotProgressionBoardLegend,
-  argTypes: {
-    items: { defaultValue: legendItems },
-  },
 } as Meta;
 
 export const Default: Story<ProgressionBoardLegendProps> = (args) => (
