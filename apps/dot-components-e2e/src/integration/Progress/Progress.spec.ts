0ecf566d5ca6279ca51c9fb2ebd9fa60aed1569c
Syed Abbas <sabbas@digital.ai>
2021-02-24 14:07:11 -0600

    add e2e testing for progress component

--- apps/dot-components-e2e/src/integration/Progress/Progress.spec.ts
+++ apps/dot-components-e2e/src/integration/Progress/Progress.spec.ts
@@ -1,20 +1,24 @@
 describe('dot-components: Progress component', () => {
-  before(() => cy.visit('/iframe.html?id=experimental-progress--default'));
+  before(() => cy.visit('/iframe.html?id=components-progress--default'));
 
   it('should have a dot- prefix', () => {
     cy.get('div').should('have.class', 'dot-progress');
   });
 
-  it('should render the component', () => {
+  it('should render a circular progress component', () => {
     cy.get('svg').should('have.class', 'MuiCircularProgress-svg');
   });
 
+  it('should be rotating', () => {
+    cy.get('div').should('have.class', 'MuiCircularProgress-indeterminate');
+  });
+  
   describe('style decisions', () => {
     it('color', () => {
       cy.get('div.dot-progress').should(
         'have.css',
         'color',
-        'rgb(61, 108, 158)'
+        'rgb(100, 154, 61)'
       );
     });
   });
