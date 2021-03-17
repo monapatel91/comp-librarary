769e2ed2cdfe3e40cfb8a24cc5391a8ee276237a
Keith Stewart <kstewart@digital.ai>
2021-03-17 10:42:47 -0500

    #325: Terminal error about the extended theme when running the demo app (#326)
    * separate the type definitions into separate files
    
    * copy type definitions to demo app
    
    * use named export of component

--- apps/demo/src/app/routes/Routes.tsx
+++ apps/demo/src/app/routes/Routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import DemoProgressionBoard from '../../demo-components/DemoProgressionBoard';
-import DemoForm from '../../demo-components/DemoForm';
+import { DemoForm } from '../../demo-components/DemoForm';
 
 export const Routes = () => {
   return (
---
+++ libs/dot-components/typings/material/index.d.ts
@@ -0,0 +1,60 @@
+import { Theme } from '@material-ui/core';
+import '@material-ui/core/styles/createPalette';
+
+declare module '@material-ui/core/styles/createPalette' {
+  interface PaletteOptions {
+    icon?: IconPaletteColorOptions;
+    layer?: LayerPaletteColorOptions;
+    progressionBoard?: ProgressionBoardColorOptions;
+  }
+
+  interface Palette {
+    icon?: IconPaletteColorOptions;
+    layer?: LayerPaletteColorOptions;
+    progressionBoard?: ProgressionBoardColorOptions;
+  }
+
+  interface LayerPaletteColorOptions {
+    n0?: string;
+    n50?: string;
+    n100?: string;
+    n200?: string;
+    n300?: string;
+    n400?: string;
+    n500?: string;
+    n600?: string;
+    n700?: string;
+    n800?: string;
+    n900?: string;
+  }
+
+  interface ProgressionBoardColorOptions {
+    board?: string;
+    boardColumn?: string;
+    boardColumnHeader?: string;
+    card?: string;
+    cardBorder?: string;
+    cardCorner?: string;
+    cardSelectedBorder?: string;
+    swimlaneColumnHeader?: string;
+  }
+
+  interface IconPaletteColorOptions {
+    checkOutline?: string;
+    checkSolid?: string;
+    errorOutlines?: string;
+    errorSolid?: string;
+    fileDotted?: string;
+    improve?: string;
+    improveHover?: string;
+    infoSolid?: string;
+    lock?: string;
+    maintain?: string;
+    maintainHover?: string;
+    pendingClock?: string;
+    rogueCommits?: string;
+    thumbsDown?: string;
+    unknown?: string;
+    unknownHover?: string;
+  }
+}
