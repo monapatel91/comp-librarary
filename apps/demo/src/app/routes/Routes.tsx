0bbffc0d84904dc1350fd9274aee28c173515800
Matt H <1242102+CWSites@users.noreply.github.com>
2021-03-23 12:07:25 -0400

    #337: add Router to sandbox app (#350)

--- apps/demo/src/app/routes/Routes.tsx
+++ apps/demo/src/app/routes/Routes.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 import DemoProgressionBoard from '../../demo-components/DemoProgressionBoard';
 import { DemoForm } from '../../demo-components/DemoForm';
 
 export const Routes = () => {
   return (
-    <>
+    <Router>
       <Route path="/" exact component={DemoProgressionBoard} />
       <Route path="/insights" exact component={DemoForm} />
-    </>
+    </Router>
   );
 };
