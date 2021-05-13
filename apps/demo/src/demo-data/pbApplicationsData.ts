53af7cfb8f967f48e78e9b32c45a49c318648842
Dragutin Miletić <37184618+dmiletic85@users.noreply.github.com>
2021-05-13 15:21:50 +0200

    Issue #528: Add application names to fix TS error messages. (#532)

--- apps/demo/src/demo-data/pbApplicationsData.ts
+++ apps/demo/src/demo-data/pbApplicationsData.ts
@@ -2,9 +2,11 @@ import { PBApplications } from '@digital-ai/dot-components';
 
 export const pbApplications: PBApplications = {
   '5ba79d5929799324b10f1111': {
+    applicationName: 'no-commits',
     waitingMessage: 'Waiting for commits',
   },
   '5ba79d5929799324b10f1222': {
+    applicationName: 'commits-without-tracking',
     waitingMessage: 'Commits are not tracked',
   },
 };
