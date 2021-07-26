65d6b4b7b845bcc8b06ea9e2f16d492ba259f468
Jamaal Long <jlong@digital.ai>
2021-07-26 13:12:34 -0500

    S-77627: Snackbar Message Provider * Moved DotSnackbarContainer inside of the DotSnackbarProvider.

---
+++ libs/dot-components/src/lib/components/dot-snackbar/DotSnackbarProvider.spec.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '../../testing-utils';
+import { DotSnackbarProvider } from './DotSnackbarProvider';
+describe('<DotSnackbarProvider/>', () => {
+  it('should render DotSnackbarProvider component', () => {
+    render(
+      <DotSnackbarProvider>
+        <div>Test</div>
+      </DotSnackbarProvider>
+    );
+    expect(screen.getByTestId('dot-snackbar-container')).toBeInTheDocument()
+  });
+});
