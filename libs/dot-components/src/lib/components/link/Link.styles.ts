a8dd03f5fb1ee554e67af8cbf4ab6c346955c0b2
Matt H <matt.haff@gmail.com>
2021-08-18 12:22:03 -0400

    S-78162: updating agility colors for link and dialog

--- libs/dot-components/src/lib/components/link/Link.styles.ts
+++ libs/dot-components/src/lib/components/link/Link.styles.ts
@@ -1,13 +1,12 @@
 import { Link } from '@material-ui/core';
 import styled, { css } from 'styled-components';
-import { agilityGreen } from '../../theme-provider/colors/light-theme-colors';
 
 export const rootClassName = 'dot-link';
 
 export const StyledLink = styled(Link)`
   ${({ theme }) => css`
     &.${rootClassName} {
-      color: ${theme.palette.product === 'agility' && agilityGreen};
+      color: ${theme.palette.product === 'agility' && '#244451'};
       cursor: pointer;
 
       &:hover :not(.MuiLink-underlineHover) {
