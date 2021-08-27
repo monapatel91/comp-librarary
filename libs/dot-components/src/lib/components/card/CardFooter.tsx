56bd6170517453af59ac1b6ecab0b108f561422c
Dragutin Miletic <dragomiletic@gmail.com>
2021-08-27 09:06:37 +0200

    Issue #632: Add 'aria-label' optional prop for 'DotCardFooter' component.

--- libs/dot-components/src/lib/components/card/CardFooter.tsx
+++ libs/dot-components/src/lib/components/card/CardFooter.tsx
@@ -4,10 +4,14 @@ import { useStylesWithRootClass } from '../useStylesWithRootClass';
 import { rootClassName, StyledDiv } from './CardFooter.styles';
 
 export interface CardFooterProps extends CommonProps {
+  /** Defines a string value that labels the current element **/
+  ariaLabel?: string;
+  /** The content for the CardFooter.*/
   children: ReactNode;
 }
 
 export const DotCardFooter = ({
+  ariaLabel,
   children,
   className,
   'data-testid': dataTestId,
@@ -15,7 +19,11 @@ export const DotCardFooter = ({
   const rootClasses = useStylesWithRootClass(rootClassName, className);
 
   return (
-    <StyledDiv className={rootClasses} data-testid={dataTestId}>
+    <StyledDiv
+      aria-label={ariaLabel}
+      className={rootClasses}
+      data-testid={dataTestId}
+    >
       {children}
     </StyledDiv>
   );
