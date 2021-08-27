a1f87aacab233382441892d181459e517427033a
Dragutin Miletic <dragomiletic@gmail.com>
2021-08-27 09:03:58 +0200

    Issue #632: Add 'aria-label' optional prop for 'DotCardContent' component.

--- libs/dot-components/src/lib/components/card/CardContent.tsx
+++ libs/dot-components/src/lib/components/card/CardContent.tsx
@@ -4,17 +4,25 @@ import { CommonProps } from '../CommonProps';
 import { useStylesWithRootClass } from '../useStylesWithRootClass';
 
 export interface CardContentProps extends CommonProps {
+  /** Defines a string value that labels the current element **/
+  ariaLabel?: string;
+  /** The content for the CardContent.*/
   children: ReactNode;
 }
 
 export const DotCardContent = ({
+  ariaLabel,
   children,
   className,
   'data-testid': dataTestId,
 }: CardContentProps) => {
   const rootClasses = useStylesWithRootClass('dot-card-content', className);
   return (
-    <CardContent classes={{ root: rootClasses }} data-testid={dataTestId}>
+    <CardContent
+      aria-label={ariaLabel}
+      classes={{ root: rootClasses }}
+      data-testid={dataTestId}
+    >
       {children}
     </CardContent>
   );
