a18bcc8844240c216ef7cf585c6025ca190c1c45
Matt H <matt.haff@gmail.com>
2021-09-01 11:44:04 -0400

    D-18424: remove unused helper

--- libs/dot-components/src/lib/components/compareSize.ts
+++ libs/dot-components/src/lib/components/compareSize.ts
@@ -7,15 +7,3 @@ export const compareWidth = (
 
   return parentWidth < childWidth;
 };
-
-export const compareWidthHeight = (
-  parentEl: Element | HTMLElement,
-  childEl: Element | HTMLElement
-) => {
-  const parentHeight = parentEl?.getBoundingClientRect().height;
-  const parentWidth = parentEl?.getBoundingClientRect().width;
-  const childHeight = childEl?.getBoundingClientRect().height;
-  const childWidth = childEl?.getBoundingClientRect().width;
-
-  return parentWidth < childWidth || parentHeight < childHeight;
-};
