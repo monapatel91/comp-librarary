b0f5be13ad8e012aeef668fcbbf0fe0667ffb0e8
Matt H <matt.haff@gmail.com>
2021-09-07 10:28:57 -0400

    D-18536: remove duplicate /styles import

--- typings/material/index.d.ts
+++ typings/material/index.d.ts
@@ -1,5 +1,3 @@
-import '@material-ui/core/styles/createPalette';
-
 declare module '@material-ui/core/styles/createPalette' {
   interface PaletteOptions {
     agilityInterface?: AgilityInterfaceOptions;
