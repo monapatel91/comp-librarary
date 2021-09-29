81a7a74a715950fb9b65441bb06f5495fcb83eb8
Dragutin Miletic <dragomiletic@gmail.com>
2021-09-29 13:53:43 +0200

    S-78164: Remove type to make DC guys happy.

--- libs/dot-components/src/lib/components/auto-complete/AutoComplete.tsx
+++ libs/dot-components/src/lib/components/auto-complete/AutoComplete.tsx
@@ -132,7 +132,7 @@ export const DotAutoComplete = ({
   const [showPlaceholder, setShowPlaceholder] = useState(
     !value && !defaultValue
   );
-  const [isOpened, setIsOpened] = useState<boolean>(false);
+  const [isOpened, setIsOpened] = useState(false);
   const rootClasses = useStylesWithRootClass(rootClassName, className);
   const textFieldRootClasses = useStylesWithRootClass(
     textFieldRootClassName,
