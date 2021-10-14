939697a6c4e9e9c6087477882551d85a21a5a992
Matt H <matt.haff@gmail.com>
2021-10-14 13:04:41 -0400

    D-18961: update dialog styles to be more explicit

--- libs/dot-components/src/lib/components/dialog/Dialog.stories.tsx
+++ libs/dot-components/src/lib/components/dialog/Dialog.stories.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
+import { DotAlertBanner } from '../alert-banner/AlertBanner';
 import { DotDialog, DialogProps } from './Dialog';
+import { DotTypography } from '../typography/Typography';
 
 export default {
   title: 'Components/Dialog',
   component: DotDialog,
   argTypes: {
-    children: { defaultValue: 'Batman is way better than superman' },
+    children: {
+      defaultValue: (
+        <>
+          <DotAlertBanner severity="error">There is an error...</DotAlertBanner>
+          <DotTypography>Batman is way better than superman</DotTypography>
+        </>
+      ),
+    },
     onCancel: {
       action: 'cancelled',
     },
