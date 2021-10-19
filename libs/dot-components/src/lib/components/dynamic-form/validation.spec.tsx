9164695fa43a895f04f7b26a582f62241c60854b
Dragutin Miletic <dragomiletic@gmail.com>
2021-10-19 12:01:01 +0200

    S-77126: Add 'isRequired' and 'minLength' unit test cases for 'getFieldValidation' function.

--- libs/dot-components/src/lib/components/dynamic-form/validation.spec.tsx
+++ libs/dot-components/src/lib/components/dynamic-form/validation.spec.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import { ValidationField } from './models';
-import { checkIfValidationApplies } from './validation';
+import {
+  ConditionControl,
+  DynamicFormValidation,
+  ValidationField,
+} from './models';
+import { checkIfValidationApplies, getFieldValidation } from './validation';
 
 describe('validation functions', () => {
   const formValues = {
@@ -10,6 +13,16 @@ describe('validation functions', () => {
     hasAccount: 'no',
     username: '',
   };
+
+  const matchingCondition: ConditionControl[] = [
+    { controlName: 'hasAccount', controlValue: 'no' },
+    { controlName: 'age', controlValue: '55' },
+  ];
+
+  const nonMatchingCondition: ConditionControl[] = [
+    { controlName: 'hasAccount', controlValue: 'no' },
+    { controlName: 'age', controlValue: '33' },
+  ];
 
   describe('checkIfValidationApplies', () => {
     const validationField: ValidationField = {
@@ -23,10 +36,7 @@ describe('validation functions', () => {
     it('should return false when at least one condition is not met', () => {
       const field: ValidationField = {
         ...validationField,
-        condition: [
-          { controlName: 'hasAccount', controlValue: 'no' },
-          { controlName: 'age', controlValue: '33' },
-        ],
+        condition: nonMatchingCondition,
       };
       const result = checkIfValidationApplies(field, formValues);
       expect(result).toBe(false);
@@ -42,13 +52,170 @@ describe('validation functions', () => {
     it('should return true when all conditions are met', () => {
       const field: ValidationField = {
         ...validationField,
-        condition: [
-          { controlName: 'hasAccount', controlValue: 'no' },
-          { controlName: 'age', controlValue: '55' },
-        ],
+        condition: matchingCondition,
       };
       const result = checkIfValidationApplies(field, formValues);
       expect(result).toBe(true);
+    });
+  });
+
+  describe('getFieldValidation', () => {
+    const requiredErrorMsg = 'Required field';
+    const minLengthErrorMsg = 'Min length validation failed';
+    const maxLengthErrorMsg = 'Max length validation failed';
+
+    const requiredOnlyValidation: DynamicFormValidation = {
+      isRequired: {
+        value: true,
+        errorMessage: requiredErrorMsg,
+      },
+    };
+
+    const validation: DynamicFormValidation = {
+      isRequired: {
+        value: true,
+        errorMessage: requiredErrorMsg,
+      },
+      minLength: {
+        value: 3,
+        errorMessage: minLengthErrorMsg,
+      },
+      maxLength: {
+        value: 6,
+        errorMessage: maxLengthErrorMsg,
+      },
+    };
+
+    describe('isRequired', () => {
+      it('should return correct object when passing in empty string when value is required', () => {
+        const result = getFieldValidation('', validation, formValues);
+        expect(result).toEqual({
+          isValid: false,
+          errorMessage: requiredErrorMsg,
+        });
+      });
+      it('should return correct object when passing in null value when value is required', () => {
+        const result = getFieldValidation(null, validation, formValues);
+        expect(result).toEqual({
+          isValid: false,
+          errorMessage: requiredErrorMsg,
+        });
+      });
+      it('should return correct object when passing in undefined value when value is required', () => {
+        const result = getFieldValidation(undefined, validation, formValues);
+        expect(result).toEqual({
+          isValid: false,
+          errorMessage: requiredErrorMsg,
+        });
+      });
+      it('should return correct object when passing in empty array when value is required', () => {
+        const result = getFieldValidation([], validation, formValues);
+        expect(result).toEqual({
+          isValid: false,
+          errorMessage: requiredErrorMsg,
+        });
+      });
+      it('should return correct object when passing in empty string when value is NOT required', () => {
+        const result = getFieldValidation('', undefined, formValues);
+        expect(result).toEqual({
+          isValid: true,
+          errorMessage: null,
+        });
+      });
+      it('should return correct object when passing in value and value is required', () => {
+        const result = getFieldValidation('abc', validation, formValues);
+        expect(result).toEqual({
+          isValid: true,
+          errorMessage: null,
+        });
+      });
+      it('should return correct object when passing in value and value is required but conditions are NOT met', () => {
+        const customValidation: DynamicFormValidation = {
+          isRequired: {
+            ...validation.isRequired,
+            condition: nonMatchingCondition,
+          },
+        };
+        const result = getFieldValidation('', customValidation, formValues);
+        expect(result).toEqual({
+          isValid: true,
+          errorMessage: null,
+        });
+      });
+      it('should return correct object when passing value is required and conditions are met', () => {
+        const customValidation: DynamicFormValidation = {
+          isRequired: {
+            ...validation.isRequired,
+            condition: matchingCondition,
+          },
+        };
+        const result = getFieldValidation('', customValidation, formValues);
+        expect(result).toEqual({
+          isValid: false,
+          errorMessage: requiredErrorMsg,
+        });
+      });
+    });
+
+    describe('minLength', () => {
+      it('should return correct object when string does not satisfy minLength validation ', () => {
+        const result = getFieldValidation('ab', validation, formValues);
+        expect(result).toEqual({
+          isValid: false,
+          errorMessage: minLengthErrorMsg,
+        });
+      });
+      it('should return correct object when array does not satisfy minLength validation ', () => {
+        const result = getFieldValidation(['1', '2'], validation, formValues);
+        expect(result).toEqual({
+          isValid: false,
+          errorMessage: minLengthErrorMsg,
+        });
+      });
+      it('should return correct object when passing in empty string when minLength validation is NOT defined', () => {
+        const result = getFieldValidation(
+          'ab',
+          requiredOnlyValidation,
+          formValues
+        );
+        expect(result).toEqual({
+          isValid: true,
+          errorMessage: null,
+        });
+      });
+      it('should return correct object when passing in value and it satisfies minLength validation', () => {
+        const result = getFieldValidation('abc', validation, formValues);
+        expect(result).toEqual({
+          isValid: true,
+          errorMessage: null,
+        });
+      });
+      it('should return correct object when passing in non-valid value but validation conditions are not met', () => {
+        const customValidation: DynamicFormValidation = {
+          minLength: {
+            ...validation.minLength,
+            condition: nonMatchingCondition,
+          },
+        };
+        const result = getFieldValidation('ab', customValidation, formValues);
+        expect(result).toEqual({
+          isValid: true,
+          errorMessage: null,
+        });
+      });
+      it('should return correct object when passing in non-valid value and validation conditions are met', () => {
+        const customValidation: DynamicFormValidation = {
+          minLength: {
+            ...validation.minLength,
+            condition: matchingCondition,
+          },
+        };
+        const result = getFieldValidation('ab', customValidation, formValues);
+        expect(result).toEqual({
+          isValid: false,
+          errorMessage: minLengthErrorMsg,
+        });
+      });
     });
   });
 });
