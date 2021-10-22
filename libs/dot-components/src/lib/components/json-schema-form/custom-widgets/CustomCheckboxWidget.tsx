4fe23b5d05cff7e34c65d705e3e8ebd112c63b00
Keith Stewart <kgstewart@gmail.com>
2021-10-22 14:38:03 -0500

    cleanup commented code and prop imports Workitem-id: S-79673

--- libs/dot-components/src/lib/components/json-schema-form/custom-widgets/CustomCheckboxWidget.tsx
+++ libs/dot-components/src/lib/components/json-schema-form/custom-widgets/CustomCheckboxWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { WidgetProps } from 'react-jsonschema-form';
 
 import { DotSwitch } from '../../switch/Switch';
@@ -9,16 +9,7 @@ export const CustomCheckboxWidget = ({
   value,
   disabled,
   onChange,
-  required,
 }: WidgetProps) => {
-  // const [error, setError] = useState(false);
-  // const [helperText, setHelperText] = useState<string>();
-
-  // useEffect(() => {
-  //   setError(rawErrors?.length > 0 ? true : false);
-  //   setHelperText(rawErrors?.length > 0 ? rawErrors[0] : null);
-  // }, [rawErrors]);
-
   return (
     <DotSwitch
       id={id}
@@ -26,9 +17,6 @@ export const CustomCheckboxWidget = ({
       checked={value}
       disabled={disabled}
       onChange={(event) => onChange(event.target.checked)}
-      // required={required}
-      // error={error}
-      // helperText={helperText}
     />
   );
 };
