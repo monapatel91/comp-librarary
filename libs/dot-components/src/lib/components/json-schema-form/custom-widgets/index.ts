f4e5098e5b2e3fb139fafed9a068a64e2804ef54
Keith Stewart <kgstewart@gmail.com>
2021-11-04 16:12:29 -0500

    remove DotJsonSchemaForm file from library Workitem-id: S-79985

--- libs/dot-components/src/lib/components/json-schema-form/custom-widgets/index.ts
+++
@@ -1,5 +0,0 @@
-export { CustomCheckboxWidget } from './CustomCheckboxWidget';
-export { CustomTextWidget } from './CustomTextWidget';
-export { CustomCheckboxesWidget } from './CustomCheckboxesWidget';
-export { CustomSelectWidget } from './CustomSelectWidget';
-export { CustomRadioWidget } from './CustomRadioWidget';
