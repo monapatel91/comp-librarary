cf8a84e5147a1a7482d82a67283db619615554a8
Matt H <matt.haff@gmail.com>
2021-11-09 10:36:53 -0500

    D-19066: add API documentation

--- libs/dot-components/src/lib/components/alert-banner/AlertBanner.tsx
+++ libs/dot-components/src/lib/components/alert-banner/AlertBanner.tsx
@@ -8,10 +8,15 @@ import { DotTypography, TypographyVariant } from '../typography/Typography';
 export type AlertBannerSeverity = 'error' | 'info' | 'success' | 'warning';
 
 export interface AlertBannerProps extends CommonProps {
+  /** The action to display. It renders after the message, at the end of the alert */
   action?: ReactNode;
+  /** The content of the component */
   children: ReactNode | string;
+  /** callback which is triggered when alert banner is closed */
   onClose?: (event: MouseEvent) => void;
+  /** will determine color and icon being used */
   severity: AlertBannerSeverity;
+  /** when specified, will control the text that is used inside the alert banner */
   textVariant?: TypographyVariant;
 }
 
