cdc164d5368b4d234b1c4e88807557dc5ffefd6e
Stephen Elsemore <root@ctf-dev-build-box.collab.net>
2021-12-07 16:30:49 -0500

    #841 Fix initial form state control with initialValue false

--- libs/dot-components/src/lib/components/dynamic-form/utils/formHelpers.tsx
+++ libs/dot-components/src/lib/components/dynamic-form/utils/formHelpers.tsx
@@ -97,7 +97,7 @@ export const getInitialStateFromControl = (
   const formStateItem: DynamicFormStateItem = { ...INITIAL_STATE_ITEM };
 
   if (hidden) formStateItem.hidden = hidden;
-  if (initialValue) {
+  if (initialValue !== null) {
     formStateItem.value = initialValue;
 
     if (liveValidation) {
