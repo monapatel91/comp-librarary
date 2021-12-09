628b59e165fbb21ac21bb6819a4d5e256014d268
Angel Gavalda <agavalda@digital.ai>
2021-12-09 13:02:46 +0100

    S-80586 add flex containers 3:D

--- apps/demo/src/demo-components/DemoBreadcrumbs.tsx
+++ apps/demo/src/demo-components/DemoBreadcrumbs.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-
-import { DotBreadcrumbs } from '@digital-ai/dot-components';
+import styled, { css } from 'styled-components';
+import {
+  DotBreadcrumbs,
+  DotButton,
+  DotChip,
+  DotIconButton,
+} from '@digital-ai/dot-components';
 import { LinkUnderline } from '../../../../libs/dot-components/src/lib/components/link/Link';
 
 export const DemoBreadcrumbs = () => {
@@ -28,7 +33,50 @@ export const DemoBreadcrumbs = () => {
         Resize screen until collapse appears, clicking on collapsed button shows
         nothing and few warnings on console
       </p>
-      <DotBreadcrumbs items={items} expansionMenu={true} />
+      <StyledDemoBreadcrumbsStyled className="action-toolbar-wrapper">
+        <div className="breadcrumbs-labels">
+          <DotBreadcrumbs items={items} expansionMenu={true} />
+          <DotChip>TEMPLATE</DotChip>
+        </div>
+        <div className="action-toolbar">
+          <div className="action-toolbar-actions">
+            <DotButton>fake button</DotButton>
+            <DotButton>another button</DotButton>
+          </div>
+          <DotIconButton
+            color="inherit"
+            data-testid="help-icon-button"
+            iconId="help"
+            iconSize="small"
+            size="medium"
+            tooltip="Open documentation page related to this page in a new browser tab"
+          />
+        </div>
+      </StyledDemoBreadcrumbsStyled>
     </>
   );
 };
+
+const StyledDemoBreadcrumbsStyled = styled.div`
+  &.action-toolbar-wrapper {
+    display: flex;
+    align-items: center;
+
+    .breadcrumbs-labels {
+      display: flex;
+      align-items: center;
+    }
+
+    .action-toolbar {
+      display: flex;
+      flex-grow: 1;
+      align-items: center;
+      justify-content: flex-end;
+
+      .action-toolbar-actions {
+        margin-left: 104px; //13*8px
+        display: flex;
+      }
+    }
+  }
+`;
