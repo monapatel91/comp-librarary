735defdefe137241300ca3d0d84673a3274cb915
Dragutin Miletic <dragomiletic@gmail.com>
2021-12-15 09:15:27 +0100

    D-19200: Extract 'NestedList' and 'ListItem' components into separate files to increase readability.

---
+++ libs/dot-components/src/lib/components/list/ListItem.tsx
@@ -0,0 +1,214 @@
+import React, {
+  ElementType,
+  MouseEvent,
+  ReactNode,
+  useEffect,
+  useState,
+} from 'react';
+import { ListItemIcon, ListItemText } from '@material-ui/core';
+import { useStylesWithRootClass } from '../useStylesWithRootClass';
+import {
+  listItemLinkClassName,
+  listItemRootClass,
+  StyledListItem,
+} from './List.styles';
+import { DotIcon } from '../icon/Icon';
+import { DotLink, LinkTarget } from '../link/Link';
+import { DotTooltip } from '../tooltip/Tooltip';
+import { DotTypography } from '../typography/Typography';
+import { getChevronIcon } from './utils/helpers';
+import { CommonProps } from '../CommonProps';
+import { PopperPlacement } from '../menu/Menu';
+import { NestedList, NestedListType } from './NestedList';
+
+export interface ListItemProps extends CommonProps {
+  /** string or JSX element that is displayed inside the list */
+  child?: ReactNode;
+  /** The component used for the root node. Either a string to use a HTML element or a component. */
+  component?: ElementType;
+  /** If true, a 1px light border is added to the bottom of the list item. */
+  divider?: boolean;
+  /** If provided, the icon ID which is displayed at the end of the list item */
+  endIconId?: string;
+  /** If provided, the list item will be rendered as a link */
+  href?: string;
+  /** DEPRECATED, DO NOT USE */
+  index?: number;
+  /* If true, it will be marked as item which has nested list opened  */
+  isOpened?: boolean;
+  /** If provided, the menu item will display a nested list */
+  items?: Array<ListItemProps>;
+  /** If nested list type is 'menu', determines the placement of the menu */
+  menuPlacement?: PopperPlacement;
+  /** If nested type is 'drawer', determines the width of the left spacing */
+  nestedDrawerLeftSpacing?: number;
+  /** If 'menu' the nested list will be displayed as a flyout nav, else it will be an expand/collapse toggle list */
+  nestedListType?: NestedListType;
+  /** Event callback */
+  onClick?: (event: MouseEvent) => void;
+  /** Menu leave event callback */
+  onMenuLeave?: () => void;
+  /** The main content element */
+  primaryText?: string;
+  /** The secondary content element */
+  secondaryText?: string;
+  /** Selected list item */
+  selected?: boolean;
+  /** If provided, the icon ID which is displayed on the front of the list item */
+  startIconId?: string;
+  /** where to open the link */
+  target?: LinkTarget;
+  /** Text which is displayed in the list item */
+  text?: string;
+  /** DEPRECATED, DO NOT USE */
+  title?: string;
+  /** Tooltip text displayed on hover */
+  tooltip?: string;
+}
+
+export const DotListItem = ({
+  ariaLabel,
+  className,
+  component = 'li',
+  'data-testid': dataTestId,
+  divider = false,
+  endIconId,
+  href,
+  isOpened,
+  onClick,
+  onMenuLeave,
+  index,
+  items = [],
+  menuPlacement,
+  nestedDrawerLeftSpacing,
+  nestedListType,
+  primaryText,
+  secondaryText,
+  selected,
+  startIconId,
+  target,
+  text,
+  title,
+  tooltip,
+}: ListItemProps) => {
+  const hasChildren = items.length > 0;
+  const isFlyout = nestedListType === 'menu' && hasChildren;
+  const [anchorEl, setAnchorEl] = useState<null | Element>(null);
+  const rootClasses = useStylesWithRootClass(
+    listItemRootClass,
+    className,
+    isOpened ? 'open' : ''
+  );
+
+  useEffect(() => {
+    // deprecation warnings
+    if (title) {
+      console.warn(
+        'The use of `title` is deprecated and will be removed in the next major release, please use `tooltip` instead.'
+      );
+    }
+    if (index) {
+      console.warn(
+        'The use of `index` is deprecated and will be removed in the next major release.'
+      );
+    }
+  }, []);
+
+  const toggleOpen = (event: MouseEvent<HTMLElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClick = (event: MouseEvent<HTMLElement>) => {
+    if (onClick) {
+      event.stopPropagation();
+      onClick(event);
+    }
+    if (component === 'li') {
+      toggleOpen(event);
+    }
+  };
+
+  const handleMenuLeave = () => {
+    setAnchorEl(null);
+    onMenuLeave();
+  };
+
+  const startIcon = (
+    <ListItemIcon>
+      <DotIcon iconId={startIconId} />
+    </ListItemIcon>
+  );
+
+  const endIcon = (
+    <ListItemIcon className="dot-list-item-end-icon">
+      <DotIcon iconId={endIconId} />
+    </ListItemIcon>
+  );
+
+  const renderListItemText = (): ReactNode =>
+    primaryText && secondaryText ? (
+      <ListItemText primary={primaryText} secondary={secondaryText} />
+    ) : (
+      <DotTypography variant="body1">{text}</DotTypography>
+    );
+
+  const renderListItemEndIcon = (): ReactNode =>
+    hasChildren ? (
+      <DotLink
+        color="inherit"
+        data-testid={`${dataTestId}-link`}
+        onClick={toggleOpen}
+        underline="none"
+      >
+        <DotIcon
+          className="toggle-display"
+          iconId={getChevronIcon(nestedListType, isOpened)}
+        />
+      </DotLink>
+    ) : (
+      endIconId && endIcon
+    );
+
+  return (
+    <>
+      <DotTooltip
+        data-testid={`${dataTestId}-tooltip`}
+        placement="top-start"
+        title={tooltip || title}
+      >
+        <StyledListItem
+          aria-label={ariaLabel}
+          button
+          classes={{ root: rootClasses }}
+          component={href && !onClick ? 'a' : component}
+          data-testid={dataTestId}
+          divider={divider}
+          href={onClick ? null : href}
+          onClick={onClick || !href ? handleClick : null}
+          selected={isFlyout ? isOpened : selected}
+          target={target}
+        >
+          <span className={listItemLinkClassName}>
+            {startIconId && startIcon}
+            {renderListItemText()}
+          </span>
+          {renderListItemEndIcon()}
+        </StyledListItem>
+      </DotTooltip>
+      {hasChildren && (
+        <NestedList
+          anchorEl={anchorEl}
+          ariaLabel="nested list"
+          items={items}
+          menuPlacement={menuPlacement}
+          nestedDrawerLeftSpacing={nestedDrawerLeftSpacing}
+          onMenuLeave={handleMenuLeave}
+          open={isOpened}
+          type={nestedListType}
+        />
+      )}
+    </>
+  );
+};
---
+++ libs/dot-components/src/lib/components/list/NestedList.tsx
@@ -0,0 +1,148 @@
+import React, { KeyboardEvent, MouseEvent } from 'react';
+import { Collapse } from '@material-ui/core';
+import { useStylesWithRootClass } from '../useStylesWithRootClass';
+import {
+  flyoutItemLinkClassName,
+  flyoutListItemClassName,
+  listItemLinkClassName,
+  listItemRootClass,
+  nestedDrawerClassName,
+  nestedListClassName,
+  StyledListItem,
+} from './List.styles';
+import { DotDrawer } from '../drawer/Drawer';
+import { DotIcon } from '../icon/Icon';
+import { DotList } from './List';
+import { DotMenu, PopperPlacement } from '../menu/Menu';
+import { DotTooltip } from '../tooltip/Tooltip';
+import { DotTypography } from '../typography/Typography';
+import { flyoutMenuClassName } from '../menu/Menu.styles';
+import { CreateUUID } from '../createUUID';
+import { CommonProps } from '../CommonProps';
+import { ListItemProps } from './ListItem';
+
+export type NestedListType = 'drawer' | 'expandable' | 'menu';
+
+export interface NestedListProps extends CommonProps {
+  /** Element that menu is attached to */
+  anchorEl?: Element;
+  /** Array of list items displayed */
+  items: Array<ListItemProps>;
+  /** If nested list type is 'menu', determines the placement of the menu */
+  menuPlacement?: PopperPlacement;
+  /** If nested type is 'drawer', determines the width of the left spacing */
+  nestedDrawerLeftSpacing?: number;
+  /** Event callback when leaving menu via tab or clicking away */
+  onMenuLeave?: (event: KeyboardEvent | MouseEvent) => void;
+  /** if true the nested list is visible */
+  open: boolean;
+  /** Index of the parent list item */
+  parentItemIndex?: number;
+  /** If 'menu' the nested list will be displayed as a flyout nav, else it will be an expand/collapse toggle list */
+  type?: NestedListType;
+}
+
+export const NestedList = ({
+  ariaLabel,
+  anchorEl,
+  items,
+  menuPlacement,
+  nestedDrawerLeftSpacing,
+  onMenuLeave,
+  open,
+  parentItemIndex,
+  type,
+}: NestedListProps) => {
+  const flyoutItemClasses = useStylesWithRootClass(
+    listItemRootClass,
+    flyoutListItemClassName
+  );
+  const flyoutSpanClasses = useStylesWithRootClass(
+    listItemLinkClassName,
+    flyoutItemLinkClassName
+  );
+
+  if (type === 'expandable') {
+    return (
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <DotList
+          ariaLabel={ariaLabel}
+          className={nestedListClassName}
+          component="div"
+          disablePadding={true}
+          items={items}
+          key={parentItemIndex}
+        />
+      </Collapse>
+    );
+  }
+
+  if (type === 'menu') {
+    const menuItems = items.map((item, index) => {
+      const { href, startIconId, target, onClick, title, tooltip, text } = item;
+      const startIcon = <DotIcon iconId={startIconId} />;
+      return {
+        children: (
+          <DotTooltip
+            key={`${parentItemIndex}-${index}-tooltip`}
+            placement="top-start"
+            title={tooltip || title}
+          >
+            <StyledListItem
+              className={flyoutItemClasses}
+              component={href && !onClick ? 'a' : null}
+              href={href}
+              key={`${parentItemIndex}-${index}`}
+              onClick={onClick}
+              target={target}
+            >
+              <span className={flyoutSpanClasses}>
+                {startIconId && startIcon}
+                <DotTypography variant="body1">{text}</DotTypography>
+              </span>
+            </StyledListItem>
+          </DotTooltip>
+        ),
+        classes: '',
+        key: String(index),
+      };
+    });
+
+    return (
+      <DotMenu
+        anchorEl={anchorEl}
+        ariaLabel={ariaLabel}
+        className={flyoutMenuClassName}
+        id={CreateUUID()}
+        key={parentItemIndex}
+        menuItemHeight="auto"
+        menuItems={menuItems}
+        menuPlacement={menuPlacement}
+        onLeave={onMenuLeave}
+        open={open}
+      />
+    );
+  }
+
+  if (type === 'drawer') {
+    return (
+      <DotDrawer
+        PaperProps={{ style: { left: `${nestedDrawerLeftSpacing}px` } }}
+        anchor="left"
+        className={nestedDrawerClassName}
+        data-testid="nested-drawer"
+        open={open}
+        variant="persistent"
+      >
+        <DotList
+          ariaLabel={ariaLabel}
+          className={nestedListClassName}
+          component="div"
+          disablePadding={true}
+          items={items}
+          key={parentItemIndex}
+        />
+      </DotDrawer>
+    );
+  }
+};
